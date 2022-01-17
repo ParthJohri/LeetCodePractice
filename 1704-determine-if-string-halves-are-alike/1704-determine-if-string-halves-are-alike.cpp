0770250792c3bb174e1b6447bb82f3b388bbc829
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-17 15:08:35 +0530

    Time: 7 ms (13.85%), Space: 6.9 MB (13.96%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     bool halvesAreAlike(string s) {
         transform(s.begin(),s.end(),s.begin(),::tolower);
+        unordered_set<char> v={'a','e','i','o','u'};
         string a=s.substr(0,s.length()/2);
         string b=s.substr(s.length()/2,s.length()/2);
         int ca=0,cb=0;
         for(auto i:a)
-            {if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')
+            {if(v.find(i)!=v.end())
                 ++ca;}
         for(auto i:b)
         {
-            if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')
+            if(v.find(i)!=v.end())
                 ++cb;
         }
         return ca==cb;
