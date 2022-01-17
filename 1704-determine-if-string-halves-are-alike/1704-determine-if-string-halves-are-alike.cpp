b2ad42ad1b3c5c8c6e13b5c31499c819c4a57f6d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-17 15:01:37 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (49.44%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool halvesAreAlike(string s) {
+        transform(s.begin(),s.end(),s.begin(),::tolower);
         string a=s.substr(0,s.length()/2);
         string b=s.substr(s.length()/2,s.length()/2);
         int ca=0,cb=0;
         for(auto i:a)
-            {if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U')
+            {if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')
                 ++ca;}
         for(auto i:b)
         {
-            if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u'||i=='A'||i=='E'||i=='I'||i=='O'||i=='U')
+            if(i=='a'||i=='e'||i=='i'||i=='o'||i=='u')
                 ++cb;
         }
         return ca==cb;
