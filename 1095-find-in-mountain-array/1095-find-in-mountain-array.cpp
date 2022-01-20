cb71cc5652b9b95ac92d3e032facb8d8c613c22a
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-20 15:44:04 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (99.45%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -0,0 +1,93 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        
+        int start=0;
+        int end=mountainArr.length()-1;
+        int mid=0;
+        int peak=0;
+        
+        while(start<end)
+        {
+            mid=start+(end-start)/2;
+            
+            if(mountainArr.get(mid)>mountainArr.get(mid+1))
+            {
+                end=mid;
+            }
+            else 
+                start=mid+1;
+        }
+        peak=start;
+        cout<<peak<<"\n";
+        
+        //first half
+        start=0;
+        end=peak;
+        mid=0;
+        bool b=mountainArr.get(start)<mountainArr.get(end);
+        
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target)
+                return mid;
+            if(b)
+            {
+                 if(target>mountainArr.get(mid))
+                    start=mid+1;
+                 else
+                    end=mid-1;
+            }
+            else
+            {
+                if(target<mountainArr.get(mid))
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }         
+        }
+        
+        //second half
+        start=peak+1;
+        end=mountainArr.length()-1;
+        mid=0;
+        b=mountainArr.get(start)<mountainArr.get(end);
+       
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(mountainArr.get(mid)==target)
+                return mid;
+            if(b)
+            {
+                 if(target>mountainArr.get(mid))
+                    start=mid+1;
+                 else
+                    end=mid-1;
+            }
+            else
+            {
+                if(target<mountainArr.get(mid))
+                    start=mid+1;
+                else
+                    end=mid-1;
+            }         
+        }
+        
+        return -1;
+        
+    }
+    
+};   
