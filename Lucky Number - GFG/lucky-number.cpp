63bd0be22320c332b644a2f212b593141d77db99
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-22 21:28:39 +0530

    Added solution - LeetHub

--- Lucky Number - GFG/lucky-number.cpp
+++ Lucky Number - GFG/lucky-number.cpp
@@ -7,29 +7,20 @@ class Solution {
   public:
     int isLuckyOrNot(long long N) {
         // code 
-        long long Num=N;
-        vector<string> v;
         int count=0;
         set<long long int> s;
-        while(Num!=0)
+        if(N<9)
+            return 1;
+        while(N!=0)
         {
-            v.push_back(to_string(Num%10));
-            s.insert(Num%10);
+            if(N%10==1)
+            return 0;
+            s.insert(N%10);
             ++count;
-            Num=Num/10;
+            N=N/10;
         }
-        long long int p=1;
-        long long int value=count*(count+1)/2;
-        for(int i=0;i<v.size();i++)
-        {p=stoi(v[i]);
-            for(int j=i+1;j<v.size();j++)
-            {
-                p*=stoi(v[j]);
-                s.insert(p);
-            }
-            
-        }
-        return s.size()==value;
+
+        return s.size()==count;
     }
 };
 
