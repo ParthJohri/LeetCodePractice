c254cd6f4fcfdc60b7d82af3c62448e38b323ebd
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-23 23:03:04 +0530

    Time: 9 ms (53.92%), Space: 15.1 MB (76.28%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
+        
+        for(int i=0;i<indices.size();i++)
+        {
+            while(indices[i]!=i)
+            {
+                swap(s[i],s[indices[i]]);
+                swap(indices[i],indices[indices[i]]);
+            }
+        }
+        return s;
+        /*
         string str=s;
         int x=0;
         for(auto i: indices)
             str[i]=s[x++];
         return str;
+        */
     }
 };
