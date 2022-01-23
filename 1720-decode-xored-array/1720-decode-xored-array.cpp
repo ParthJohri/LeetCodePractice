745ee0ce573cb53a0f15cbefbe698e0fe3558a3f
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-24 04:37:37 +0530

    Time: 59 ms (13.74%), Space: 24.8 MB (94.52%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int> v(encoded.size()+1);
+        vector<int> v(encoded.size()+1,0);
         v[0]=first;
         for(int i=0;i<encoded.size();i++)
             v[i+1]=v[i]^encoded[i];
