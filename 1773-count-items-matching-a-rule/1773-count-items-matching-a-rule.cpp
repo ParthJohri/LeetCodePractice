e3464ed7f8f36252bce3844ea878f4cad90ff77b
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-24 14:43:14 +0530

    Time: 60 ms (98.71%), Space: 31 MB (43.13%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
+
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        int count=0;
-        for(auto i: items)
-        {
-            if(ruleKey=="color")
-            {
-                if(ruleValue==i[1])
-                    ++count;
-            }
-            else if(ruleKey=="type")
-            {
-                if(ruleValue==i[0])
-                    ++count;
-            }
-            else if(ruleKey=="name")
-            {
-                if(ruleValue==i[2])
-                    ++count;
-            }
-        }
-        return count;
+        int index=ruleKey=="type"?0:ruleKey=="color"?1:2;//finding the asked index
+        // using count_if function of algorithm header file
+        // syntax count_if(start,end,condition);
+        // https://www.tutorialspoint.com/count-if-in-cplusplus-stl
+        // [&] means call by reference
+        // [&] (auto i){ return i[index]==ruleValue is a lambda function, it a one liner function}
+        return count_if(items.begin(),items.end(),[&](auto &i){return i[index]==ruleValue;});
     }
 };
