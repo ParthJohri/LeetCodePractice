eefe87a1e3544b067cee02a5804b9f111e6aafc8
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-24 15:03:54 +0530

    Time: 3 ms (57.66%), Space: 5.9 MB (69.07%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int sum(int n)
     {
-        int s=0,x=1;
-        while(x<=n/2)
+        int s=1,x=2;
+        while(x*x<n)
         {
             if(n%x==0)
-                s+=x;
+                {s+=x;
+                 s+=n/x;}
             x++;
         }
-        return s;
+        return n!=1?s:0;
     }
     bool checkPerfectNumber(int num) {
+        cout<<sum(num);
         return num==sum(num);
     }
 };
