47ed5806118a4235aa2d73481b90c69787a60a5a
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-24 22:37:07 +0530

    Time: 12 ms (37.78%), Space: 13.6 MB (64.11%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    int binarysearch(vector<int> nums,int target, bool beta)
-    {
-        int start=0;
-        int end=nums.size()-1;
-        int mid=0;
-        int ini=-1;
-        while(start<=end)
-        {
-            mid=start+(end-start)/2;
-            if(nums[mid]==target)
-               {ini=mid;
-               if(beta)
-               {
-                   end=mid-1;
-               }
-                else
-                {
-                    start=mid+1;
-                }
-               }
-            else if(target>nums[mid])
-                start=mid+1;
-            else
-                end=mid-1;
-        }
-        return ini;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-
-        vector<int> pos(2,0);
-        pos[0]=binarysearch(nums,target,true);
-        pos[1]=binarysearch(nums,target,false);
-        return pos;
+        int first=-1,last=-1;
+        int low=0,high=nums.size()-1,mid=0;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(nums[mid]>target)
+                high=mid-1;
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+            {
+                if(mid==0||nums[mid]!=nums[mid-1])
+                    {first=mid;break;}
+                else
+                    high=mid-1;
+            }
+        }
+        low=0,high=nums.size()-1,mid=0;
+        while(low<=high)
+        {
+            mid=low+(high-low)/2;
+            if(nums[mid]>target)
+                high=mid-1;
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+            {
+                        if(mid==nums.size()-1||nums[mid]!=nums[mid+1])
+                    {last=mid;break;}
+                else
+                    low=mid+1;
+            }
+        }
+        return {first,last};
     }
 };
