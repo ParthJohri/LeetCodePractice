3ac5654abebfed770e46852ad53c5ad618024e12
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-27 05:28:33 +0530

    Time: 13 ms (16.10%), Space: 9.8 MB (82.63%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.cpp
@@ -8,5 +8,16 @@ public:
                 ++count;
         }
         return count;
-    }
+            }
 };
+        /*Good Solution
+        There are many great solutions already posted similar to this. The idea is simple - use log base 10 to         get number of digits and check for even/odd. Just a small catch here - log base 10 of a number gives           the number of digits - 1.
+        Example -(int)log10(9) = 0
+        So if number of digits is even, log10(n) will be odd. Hence, we just need to do bitwise AND of                 log10(n) and 1 for odd check.
+
+        int findNumbers(vector<int>& nums) {
+        int res=0;
+        for(auto num:nums) {
+            res+=(int)log10(num)&1;
+        }
+        return res;}*/
