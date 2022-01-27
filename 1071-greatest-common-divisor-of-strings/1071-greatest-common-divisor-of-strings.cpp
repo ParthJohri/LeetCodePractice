3bd4cc4183427ef20c2350a47d77869fd0669316
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-27 23:47:02 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (62.80%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -4,7 +4,6 @@ public:
         int l1,l2;
         l1=str1.length();
         l2=str2.length();
-        if(l1==l2&&str1!=str2) return "";
         string s=str1.substr(0,__gcd(l1,l2));
         string s1="",s2="";
         while(s1.length()<l1)
