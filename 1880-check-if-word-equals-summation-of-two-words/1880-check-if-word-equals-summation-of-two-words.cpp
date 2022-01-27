d612fee49720fec9513b912cf3d3a18a35608400
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-27 05:46:20 +0530

    Time: 4 ms (15.71%), Space: 5.8 MB (96.36%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.cpp
@@ -8,9 +8,6 @@ public:
             s2+=i-'1';
         for(auto i:targetWord)
             s3+=i-'1';
-        int n1=stoi(s1);
-        int n2=stoi(s2);
-        int n3=stoi(s3);
         return (stoi(s1)+stoi(s2)==stoi(s3));
     }
 };
