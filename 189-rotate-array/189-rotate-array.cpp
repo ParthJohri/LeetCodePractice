13643763730b7464abbc3d773834bda4fb5156c2
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-01-31 15:56:17 +0530

    Time: 36 ms (52.36%), Space: 25 MB (53.08%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         k=k%nums.size();
         reverse(nums.begin(),nums.end());
-        reverse(nums.begin()+k,nums.end());
         reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
