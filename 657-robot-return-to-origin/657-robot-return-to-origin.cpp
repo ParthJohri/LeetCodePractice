27a77ccc0ac9c984de049ce7791c94fd2f818149
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-01 02:37:21 +0530

    Time: 18 ms (20.67%), Space: 8 MB (79.83%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+
+        int sx=0,sy=0;
+        for(auto i: moves)
+        {
+            if(i=='L')
+                sx-=1;
+            else if(i=='R')
+                sx+=1;
+            if(i=='U')
+                sy+=1;
+            else if(i=='D')
+                sy-=1;
+        }
+        return (sx==0&&sy==0);
+    }
+};
