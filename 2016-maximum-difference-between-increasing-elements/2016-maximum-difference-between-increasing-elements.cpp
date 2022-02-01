e70d93acb0c5a2e1fd2aee899edcbabf6639f080
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-01 15:59:51 +0530

    Time: 4 ms (78.38%), Space: 8.3 MB (37.78%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -2,6 +2,11 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int left=0,right=1,mx=0,mxmx=-1;
+        // Two Pointer Approach
+        // left will store the left pointer
+        // right will store the right pointer
+        // mx will store the maximum pointer
+        // mxmx will store the maximum of maximum number
         while(right<nums.size())
         {
             if(nums[left]<nums[right])
