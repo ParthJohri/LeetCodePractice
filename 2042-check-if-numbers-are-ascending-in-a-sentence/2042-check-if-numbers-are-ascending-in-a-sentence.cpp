d40b4bc30b0d9261279aa95ed6bd5b97a1768f3f
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-05 04:05:28 +0530

    Time: 4 ms (26.76%), Space: 6.4 MB (32.54%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isNumber(const string &s)
+    {
+        for(auto i:s)
+        {if(isdigit(i)==0)
+         return false;}
+        return true;
+    }
+    bool areNumbersAscending(string s) {
+        stringstream str(s);
+        vector<int> st;
+        string temp;
+        while(str>>temp)
+        {
+            if(isNumber(temp))
+            {int t=stoi(temp);
+             st.push_back(t);}
+        }
+        cout<<st.size()<<endl;
+        int count=0;
+        for(int i=0;i<st.size()-1;i++)
+        {
+            if(st[i]<st[i+1])
+                ++count;
+        }
+        cout<<count<<endl;
+        return count==st.size()-1;
+    }
+};
