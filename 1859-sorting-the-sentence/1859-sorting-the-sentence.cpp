0a57bb5e4a616fa8d70ce4539f8197f4ea80f39e
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-05 01:06:57 +0530

    Time: 4 ms (16.49%), Space: 7.8 MB (6.47%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+
+	stringstream str(s);
+	// Creating a string stream object
+	// Now this object can read the string
+	// as if it were a stream
+
+	vector<string> v(1000);
+	// vector to store the words of the string 
+	// or the token of the string
+
+	string temp="";
+
+	while(getline(str,temp,' '))
+	{
+		v[temp[temp.length()-1]]=temp.substr(0,temp.length()-1);
+	}
+
+    string newstr="";
+	for(auto i:v)
+	{
+		if(i!="")
+		newstr+=i+" ";
+	}
+	return newstr.substr(0,newstr.length()-1);
+    }
+};
