82284ecfae9738957b77866f4280e3ec89045ba3
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-07 05:02:12 +0530

    Time: 6 ms (7.70%), Space: 6.3 MB (78.40%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countPoints(string rings) {
+        unordered_map<int,string> v;
+        int count=0;
+        for(int i=1;i<rings.length();i+=2)
+        {
+            int bar=rings[i]-'0';
+            string val="";
+            if(rings[i-1]=='B')
+                val+="B";
+            else if(rings[i-1]=='R')
+                val+="R";
+            else
+                val+="G";
+            v[bar]+=val;
+        }
+        for(auto i:v)
+        {
+            cout<<"first "<<i.first<<" second "<<i.second<<" ";
+            bool flag1,flag2,flag3; 
+            flag1=flag2=flag3=false;
+            for(auto j: i.second)
+            {
+                if(j=='B')
+                    flag1=true;
+                else if(j=='G')
+                    flag2=true;
+                else if(j=='R')
+                    flag3=true;
+            }
+            if(flag1&&flag2&&flag3)
+                ++count;
+        }
+        return count;
+    }
+};
