a789cb0b7589333f42a0fed5f30dfe4771faf398
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-08 04:55:01 +0530

    Time: 6 ms (11.76%), Space: 7.5 MB (36.97%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int ans=1,sum=0;
-        vector<int> prefix;
-        
-        for(auto i:nums)
-            {sum+=i;
-             prefix.push_back(sum);}
-        
-        for(ans=1;;ans++)
-        {int count=0;
-            for(int i=0;i<prefix.size();i++)
-            {
-                if(ans+prefix[i]>=1)
-                    ++count;
-            }
-         if(count==prefix.size())
-             break;
+        int minval=INT_MAX,prefixsum=0;
+        for(auto i: nums)
+        {
+            prefixsum+=i;
+            minval=min(minval,prefixsum);
         }
-        return ans;
+        return minval>0?1:abs(minval)+1;
     }
 };
