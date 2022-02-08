fb57ab131bb171f391ce9ead76e3eff4e75b2467
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-09 05:01:45 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (27.89%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.cpp
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // To check if the string is 0 00 000 0000 00000 000000 0000000 ...
+    // All are 0
     int ascii(string &s)
     {
         int val=0;
@@ -9,14 +11,18 @@ public:
         }
         return val;
     }
+    // Removing Initial Zeroes 
     string rmzeros( string &str)
     {
         string nstr="";
+        // Only for string of number 0
         if(str=="0"||ascii(str)==0)
             return "0";
+        // for Normal string of nummber N
         else if(str[0]!='0')
             return str;
         else
+        // Removing the Zeroes by using substring method
         {int pos=0;
             for(int i=0;i<str.length();i++)
             {
@@ -24,30 +30,34 @@ public:
                 {pos=i;
                  break;}
             }
-                nstr=str.substr(pos,str.length());
+                nstr=str.substr(pos,str.length()); // Removing the Initial Zeroes
         }
         return nstr;
     }
     int numDifferentIntegers(string word) {
+        // Replacing all the characters with space
         for(int i=0;i<word.length();i++)
         {
             if(isalpha(word[i]))
                 word[i]=' ';
         }
         word+=' ';
+        // Creating unordered set for O(1) Average Case and O(N) Worst Case
         unordered_set<string> s;
         string temp="";
         
         for(auto i: word)
         {
+            // Extracting the words of the string
             if(i!=' ')
                 temp+=i;
             else
-                {cout<<temp<<" ";
+                {
                 if(temp!="")
-                s.insert(rmzeros(temp));
+                s.insert(rmzeros(temp));// Removing initial zeroes of the string
                 temp="";}
         }
+        // Reutrning the number of Unique Elments in a Set
         return s.size();
     }
 };
