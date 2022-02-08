53a17a7eef7148061f94652991321cae6f4e0ef8
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-02-08 06:01:32 +0530

    Time: 3 ms (92.65%), Space: 10.8 MB (5.71%) - LeetHub

--- 819-most-common-word/819-most-common-word.cpp
+++ 819-most-common-word/819-most-common-word.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     string mostCommonWord(string paragraph, vector<string>& banned) {
+        // Map to store the frequency of the words
         unordered_map<string,int> m;
-        string delimeters="!?',;.' '";
+        // To store the temp word and 
+        // the most frequenct used word after banned word
         string temp="",word="";
+        // To store all the words
         vector<string> token;
+        // Adding a space to extract last word of the paragraph
         paragraph+=" ";
+        // Extracting words from the paragraphs
         for(auto i:paragraph)
         {
             if(isalpha(i))
@@ -15,21 +20,29 @@ public:
                  temp="";
             continue;}
         }
-        int mx=0;
+        // Counting the frequency of each word 
+        // in the paragraph
         for(auto i: token)
         {
             m[i]++;
         }
+        // Removing the word which are banned 
+        // from the paragraph
         for(auto i:banned)
         {
             m[i]=0;
         }
+        // To store the maximum frequency of the word
+        int mx=0;
         for(auto i:m)
         {
+            // There will be null values in the string 
+            // because of multiple delimiters
             if(i.second>mx&&i.first!="")
                 {mx=i.second;
-                word=i.first;}
+                 word=i.first;}
         }
+        // Returning the most frequent word afterbanned
         return word;
     }
 };
