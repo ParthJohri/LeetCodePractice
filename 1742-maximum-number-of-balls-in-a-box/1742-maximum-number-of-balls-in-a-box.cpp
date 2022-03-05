6da8bcbba2e4a7177a4f557d40df22b3cde6105c
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-03-05 22:22:20 +0530

    Time: 27 ms (47.03%), Space: 49.8 MB (5.20%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sod(int n)
+    {
+        int sum=0;
+        while(n!=0)
+        {
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    int countBalls(int lowLimit, int highLimit) {
+        vector<int>s(1e5,0);
+        int lar=INT_MIN;
+        for(int i=lowLimit;i<=highLimit;i++)
+        {
+            s[sod(i)]++;
+            if(s[sod(i)]>lar)
+                lar=s[sod(i)];
+        }
+        return lar;
+    }
+};
