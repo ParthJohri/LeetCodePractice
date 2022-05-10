3589427617d55ccc211b9073a6146ca5616cb628
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-10 14:37:03 +0530

    Time: 17 ms (59.08%), Space: 17.9 MB (35.59%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-      stack<int> s;
-      for(int i=0;i<asteroids.size();i++)
-      {
-          if(s.empty()||asteroids[i]>0)
-              s.push(asteroids[i]);
-          else
-          {
-              while(true)
-              {
-                  if(s.top()<0)
-                      {s.push(asteroids[i]);break;}
-                  else if(s.top()==-asteroids[i])
-                  {s.pop();break;}
-                  else if(s.top()>-asteroids[i])
-                  {
-                      break;
-                  }
-                  else
-                  {
-                      s.pop();
-                      if(s.empty())
-                      {s.push(asteroids[i]);
-                       break;}
-                  }
-              }
-          }
-      }
-              vector<int> ans(s.size());
-
-        for(int i=s.size()-1;i>=0;i--)
+        stack<int> s;
+        for(auto i:asteroids)
         {
-            ans[i]=s.top();
+            if(i>0||s.empty())
+                s.push(i);
+            else
+            {while(true)
+            { if(s.top()<0)
+                {s.push(i);
+                break;}
+                else if(s.top()==-i)
+                {s.pop();
+                break;}
+                else if(s.top()>-i)
+                {break;}
+                else
+                {
+                    s.pop();
+                    if(s.empty())
+                    {
+                        s.push(i);
+                        break;
+                    }
+                }
+            }
+        }
+        }
+        int n=s.size();
+        vector<int> ans(n);
+        for(int i=0;i<n;i++)
+        {
+            ans[n-i-1]=s.top();
             s.pop();
         }
         return ans;
