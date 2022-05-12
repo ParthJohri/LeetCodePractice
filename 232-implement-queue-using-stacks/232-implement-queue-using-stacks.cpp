cfe266ee5915b892cfad13257eb55bd9b9830ad6
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-12 16:26:48 +0530

    Time: 0 ms (100.00%), Space: 7 MB (56.70%) - LeetHub

--- 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.cpp
@@ -1,6 +1,15 @@
 class MyQueue {
 public:
     stack<int> s1,s2;
+//  Do you know when we should use two stacks to implement a queue?
+
+// I was asked in the internship interview of a company two years ago.
+
+// The application for this implementation is to separate read & write of a queue in multi-processing. One of the stack is for read, and another is for write. They only interfere each other when the former one is full or latter is empty. 
+
+//  When there's only one thread doing the read/write operation to the stack, there will always one stack empty. However, in a multi-thread application, if we have only one queue, for thread-safety, either read or write will lock the whole queue. In the two stack implementation, as long as the second stack is not empty, push operation will not lock the stack for pop.
+
+// This is not only a trick. :)
     MyQueue() {
         
     }
