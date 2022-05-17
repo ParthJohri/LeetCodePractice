919e1a1e2b0e7c6223368d517d573ce972a590ee
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-17 11:12:20 +0530

    Time: 15 ms (36.48%), Space: 19 MB (14.48%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -12,9 +12,24 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int count=0;
         if(root==NULL) return 0;
-        int lh= maxDepth(root->left);    
-        int rh=maxDepth(root->right);
-        return 1+max(lh,rh);
+        while(!q.empty())
+        {
+            int size=q.size(); // Always use a variable to store the value of queue because here it is changing and q.size() will give wrong results
+            ++count;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* curr=q.front();
+                q.pop();
+                if(curr->left!=NULL)
+                    q.push(curr->left);
+                if(curr->right!=NULL)
+                    q.push(curr->right);
+            }
+        }
+        return count;
     }
 };
