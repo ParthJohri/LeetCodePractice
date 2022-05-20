d4fe9c0aa362f7ba7fcc8ed767232c1d2fdca0c0
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-21 01:24:24 +0530

    Time: 327 ms (17.28%), Space: 104.6 MB (34.65%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -7,16 +7,19 @@ public:
         int count=0;
         for(auto i:v)
         {
-            int c=i.second;
+// Make sure you use double data type here
+            double c=i.second;
             if(c==1)
                 return -1;
             else
-                {
-                if(c%3==0)
-                    count+=c/3;
-                else
-                    count+=c/3+1;
-               }
+                count+=ceil(c/3);
+//                 Better to use ceil function
+//                 {
+//                 if(c%3==0)
+//                     count+=c/3;
+//                 else
+//                     count+=c/3+1;
+//                }
         }
         return count;
     }
