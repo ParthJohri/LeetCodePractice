a0346ac4b88e665f6d008dd28d3708690520edca
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-22 02:47:37 +0530

    Time: 8 ms (38.29%), Space: 10.4 MB (52.20%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        map<int,int> m;
+        int val=nums[0].length();
+        int mx=(int)pow(2,val);
+
+        for(auto i:nums)
+        {
+            int v=i.length(),ans=0,x=0;
+            while(--v>=0)
+            {
+                ans+=(i[v]-'0')*(int)pow(2,x++);
+            }
+            m[ans]++;
+        }
+
+        for(int i=0;i<mx;i++)
+        {
+            if(m[i]==0)
+            {
+                if(i==0)
+                    {
+                    string a="";
+                        while(val--)
+                        {
+                            a+="0";
+                        }
+                        return a;
+                    }
+                else
+                {
+                    string a="";
+                    while(i!=0)
+                    {
+                        a=to_string(i%2)+a;
+                        i=i/2;
+                        val--;
+                    }
+                    while(val--)
+                    {
+                        a="0"+a;
+                    }
+                    return a;
+                }
+            }
+        }
+        return "";
+    }
+};
