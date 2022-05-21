dd213b70e6757a7c4b1583c601293da42e984e7b
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-21 22:56:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (44.70%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string convertToTitle(int n) {
     string ans="";
-        while(n--)
+        while(n--) // Important check the discuss section since excel stores it in a 1 to 26 system and not in 0 to 25 system
         {
             ans=char(n%26+'A')+ans;
             n/=26;
