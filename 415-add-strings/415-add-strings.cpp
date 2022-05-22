1afdd9f0db9fd02cce9f892e760680eeb2b54b9d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-22 18:58:13 +0530

    Time: 61 ms (5.26%), Space: 72.3 MB (5.43%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+       int n1=num1.length();
+       int n2=num2.length();
+       int v=max(n1,n2);
+       if(v-n1>0)
+       {
+           int c=v-n1;
+           while(c--)
+           {
+               num1="0"+num1;
+           }
+       }
+       if(v-n2>0)
+        {
+            int c2=v-n2;
+           while(c2--)
+           {
+               num2="0"+num2;
+           }
+        }
+        cout<<num1<<" "<<num2<<endl;
+        string s;
+        int sum=0,car=0;
+        for(int i=v-1;i>=0;i--)
+        {
+             sum=(num2[i]-'0'+num1[i]-'0'+car);
+             car=sum/10;
+             sum=sum%10;
+             s=to_string(sum)+s;
+        }
+        if(car!=0)
+        s=to_string(car)+s;
+        return s;
+    }
+};
