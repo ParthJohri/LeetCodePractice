86a3b8a26fff2dab25dc3190aa879e65bc1cdc82
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-27 16:42:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.01%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int minFlips(int a, int b, int c) {
         int count=0;
+        // Note: The popcount function is a C++ 20 standard builtin function that counts set bits. LeetCode uses g++ compiler with the C++17 standard so we can use __builtin_popcount instead. For other compilers please use bitset<32>().count().
         while(c!=0 || a!=0 || b!=0)
         {
             if( (a&1)!=0 && (b&1)!=0 && (c&1)==0)
