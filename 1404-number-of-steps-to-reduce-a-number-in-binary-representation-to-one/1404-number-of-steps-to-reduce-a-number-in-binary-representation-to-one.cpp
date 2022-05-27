a5f265ba9ea0344148114392d70d32e69dd2661e
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-27 18:06:30 +0530

    Time: 48 ms (5.15%), Space: 11.5 MB (6.25%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    void add(string &s)
+    {
+        int n=s.length();
+        string ad="";
+        while(--n)
+        {
+            ad+="0";
+        }
+        ad+="1";
+        bool b=false;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s[i]=='1'&&ad[i]=='1')
+               { 
+                 s[i]='0';
+                 b=true;
+               }
+            else if(b)
+            {
+                if(s[i]=='1' &&ad[i]=='0')
+                    {s[i]='0';
+                    b=true;}
+                else if(s[i]=='1'&&ad[i]=='1')
+                {
+                    s[i]='1';
+                    b=true;
+                }
+                else
+                    {s[i]='1';
+                    b=false;}
+            }
+            else if(s[i]=='0'&&ad[i]=='0')
+                s[i]='0';
+            else
+                s[i]='1';
+        }
+        if(b)
+            s="1"+s;
+    }
+    int numSteps(string s) {
+        int count=0;
+        while(s!="1")
+        {
+            if(s[s.length()-1]=='1')
+            {
+                add(s);
+            }
+            else if(s[s.length()-1]=='0')
+                s=s.substr(0,s.length()-1);
+            cout<<s<<endl;
+            count++;
+        }
+        return count;
+    }
+};
