c63d538775e344ea85680f046b4439e9f0846cc0
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-28 10:54:30 +0530

    Time: 3 ms (42.65%), Space: 6.2 MB (81.92%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
-        int count=0;
+        int not_closed=0,not_opened=0;
         for(auto i:s)
         {
-            if(st.empty()&&i!=')')
-                st.push('(');
-            else if(st.empty()&&i==')')
-               ++count;
-            else if(i=='(')
-                st.push('(');
-            else if(st.top()=='('&&i==')')
-                st.pop();
+            if(i=='(')
+                not_closed++;
+            else if(i==')' && not_closed>0)
+                not_closed--;
             else
-                ++count;
+                not_opened++;
         }
-        return count+st.size();
+        return not_closed+not_opened;
+        // Time Complexity: O(N);
+        // Space Complexity: O(N);
+        // stack<char> st;
+        // int count=0;
+        // for(auto i:s)
+        // {
+        //     if(st.empty()&&i!=')')
+        //         st.push('(');
+        //     else if(st.empty()&&i==')')
+        //        ++count;
+        //     else if(i=='(')
+        //         st.push('(');
+        //     else if(st.top()=='('&&i==')')
+        //         st.pop();
+        //     else
+        //         ++count;
+        // }
+        // return count+st.size();
     }
 };
