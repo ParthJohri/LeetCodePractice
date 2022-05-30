2919eacabc3c6d92dd7ed36133e93e3205187661
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-05-30 23:53:49 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.45%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -5,7 +5,6 @@ public:
         while(n>0)
         {              
             p=log10(n)/log10(3);
-            cout<<log(abs(n))/log(3)<<" "<<p<<"\n";
             if(temp==p)
                 return false;
             n-=(int)pow(3,p);
