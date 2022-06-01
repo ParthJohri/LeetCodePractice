fd238c6c23021e4c48105e7ec2fd4e420cfa4cc8
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-02 00:56:15 +0530

    Time: 30 ms (15.31%), Space: 17.5 MB (16.64%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.cpp
+++ 766-toeplitz-matrix/766-toeplitz-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        //1st Solution
         for(int i=0;i<matrix.size()-1;i++)
         {
             for(int j=0;j<matrix[0].size()-1;j++)
@@ -10,5 +11,15 @@ public:
             }
         }
         return true;
+        // 2nd Solution
+        // for(int i=1;i<matrix.size();i++)
+        // {
+        //     for(int j=1;j<matrix[0].size();j++)
+        //     {
+        //         if(matrix[i][j]!=matrix[i-1][j-1])
+        //             return false;
+        //     }
+        // }
+        // return true;
     }
 };
