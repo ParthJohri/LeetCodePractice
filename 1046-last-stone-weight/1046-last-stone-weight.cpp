da335d6f2200172483009a35e3e56c9e2c7075b6
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-08 20:57:41 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (97.68%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> p;
+        for(auto i:stones) p.push(i);
+        while(p.size()!=1)
+        {
+            int t=p.top();
+            p.pop();
+            int s=p.top();
+            p.pop();
+            p.push(abs(t-s));
+        }
+            return p.top();
+    }
+};
