6cd937f49ddbf1e32d7e3f4950ef4e35de3bbb2d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-15 21:02:01 +0530

    Time: 19 ms (5.65%), Space: 8.4 MB (84.99%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int wateringPlants(vector<int>& plants, int capacity) {
+        int count=0,cap=capacity;
+        for(int i=0;i<plants.size()-1;i++)
+        {
+            if(capacity>=plants[i])
+            {
+                capacity-=plants[i];
+                count++;
+            }
+            if(capacity<plants[i+1])
+            {
+                count+=2*(i+1);
+                capacity=cap;
+            }
+            cout<<count<<"\t";
+        }
+        return count+1;
+    }
+};
