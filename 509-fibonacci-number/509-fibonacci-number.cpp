5f17107fc4dbd6a8891571f3ec92023b97bfe4e4
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-26 22:57:15 +0530

    Time: 0 ms (100.00%), Space: 6 MB (41.21%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
+private:
+    int dp[31]={0};
 public:
     int fib(int n) {
-        int a=0,b=1,c=1;
-        if(n==0)
-            return 0;
-        else if(n==1||n==2)
-            return 1;
-        for(int i=2;i<=n;i++)
-            {c=a+b;
-             a=b;
-             b=c;}
-        return c;
+        dp[0]=0;
+        dp[1]=1;
+        dp[2]=1;
+        dp[3]=2;
+        dp[4]=3;
+        for(int i=5;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+        return dp[n];
     }
 };
