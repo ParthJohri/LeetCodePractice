5c1f28797322f2aa72ba181fbc8ee6d9104cc645
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-27 14:02:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (19.28%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
+    int fib(vector<int> &dp,int n) {
+        if(n==0) return 0;
+        if(n==1||n==2) return 1;
+        if(n==3) return 2;
+        if(n==4) return 4;
+        if(dp[n]!=0) return dp[n];
+        dp[n]=fib(dp,n-1)+fib(dp,n-2)+fib(dp,n-3);
+        return dp[n];
+    }
     int tribonacci(int n) {
         vector<int> dp(n+5,0);
+        return fib(dp,n);
         dp[0]=0;
         dp[1]=1;
         dp[2]=1;
