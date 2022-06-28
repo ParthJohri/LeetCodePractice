31ef9cfece4ad077cc8391a94075f83a96f90655
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-06-28 23:37:39 +0530

    Time: 330 ms (25.97%), Space: 121.2 MB (69.93%) - LeetHub

---
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDeletion(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int count=0;
+        cout<<n;
+        for(int i=0;i<n-1;i++)
+        {
+            if((i-count)%2==0 && nums[i+1]==nums[i]) ++count;
+        }
+        if(nums[n-2]==nums[n-1] &&(n-1-count)%2==0) ++count;
+        if((n-count)%2!=0) ++count;
+        return count;
+    }
+};
