3305131cc69cd78d17d2af39071b2616cfc86a99
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-04 02:16:31 +0530

    Time: 2 ms (58.76%), Space: 5.7 MB (95.03%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int trailingZeroes(int n) {
         int count=0;
-        // Formula for trailing zeros = n/5 + n/25 + n/125 + n/625
+        // Formula for trailing zeros = n/5 + n/25 + n/125 + n/625 + .......
         for(int i=5;i<=n;i*=5)
             count = count + n/i;
         return count;
