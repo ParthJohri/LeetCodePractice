ffe05da948b110014969d304204cb17837d5f9d0
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-05 14:31:53 +0530

    Time: 192 ms (99.03%), Space: 81.8 MB (87.38%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        unordered_set<int> s;
+        // unordered_set<int> s;
         bitset<200001> b; // Uses less space, can also be used to convert decimal to binary
-        for(auto i:candyType) s.insert(i), b.set(i+100000);
+        for(auto i:candyType) {
+            // s.insert(i);
+            b.set(i+100000);}
         return min(b.count(),candyType.size()/2);
         // return min(s.size(),candyType.size()/2);
     }
