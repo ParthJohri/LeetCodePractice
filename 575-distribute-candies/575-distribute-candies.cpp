f7ecfc58fd952d46410a93c065e03e56031f862e
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-05 14:31:57 +0530

    Time: 274 ms (89.20%), Space: 81.7 MB (87.38%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -5,7 +5,8 @@ public:
         bitset<200001> b; // Uses less space, can also be used to convert decimal to binary
         for(auto i:candyType) {
             // s.insert(i);
-            b.set(i+100000);}
+            b.set(i+100000);
+        }
         return min(b.count(),candyType.size()/2);
         // return min(s.size(),candyType.size()/2);
     }
