e920ec0eb75149a75581f8b72e3cbf835e700ba3
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-07 04:55:19 +0530

    Time: 260 ms (93.57%), Space: 123.2 MB (40.16%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> v(nums);
-        sort(v.begin(),v.end());
-        int n=nums.size();
-        n=n%2==0?n-2:n-1;
-        int x=0;
-        for(int i=n;i>=0;i-=2)
+        
+        for(int i=1;i<nums.size()-1;i++)
         {
-            nums[i]=v[x++];
+            if(nums[i-1]+nums[i+1]==2*nums[i])
+                swap(nums[i],nums[i+1]);
         }
-        n=nums.size();
-        n=n%2==0?n-1:n-2;
-        for(int i=n;i>=1;i-=2)
+        for(int i=nums.size()-2;i>=1;i--)
         {
-            nums[i]=v[x++];
+            if(nums[i-1]+nums[i+1]==2*nums[i])
+                swap(nums[i],nums[i-1]);
         }
+        // Wiggle Sort 
+        // Time Complexity : O(NlogN)
+        // Space Complexity : O(N)
+        // vector<int> v(nums);
+        // sort(v.begin(),v.end());
+        // int n=nums.size();
+        // n=n%2==0?n-2:n-1;
+        // int x=0;
+        // for(int i=n;i>=0;i-=2)
+        // {
+        //     nums[i]=v[x++];
+        // }
+        // n=nums.size();
+        // n=n%2==0?n-1:n-2;
+        // for(int i=n;i>=1;i-=2)
+        // {
+        //     nums[i]=v[x++];
+        // }
         return nums;
     }
 };
