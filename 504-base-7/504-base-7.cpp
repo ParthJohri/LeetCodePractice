60b95ab7c6b7e3eafa530d3102994545fcefa46d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-08 13:18:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (7.73%) - LeetHub

--- 504-base-7/504-base-7.cpp
+++ 504-base-7/504-base-7.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     string convertToBase7(int num) {
+//         Decimal to Other Base System
+//         Step 1 − Divide the decimal number to be converted by the value of the new base.
+
+//         Step 2 − Get the remainder from Step 1 as the rightmost digit (least significant digit) of new base number.
+
+//         Step 3 − Divide the quotient of the previous divide by the new base.
+
+//         Step 4 − Record the remainder from Step 3 as the next digit (to the left) of the new base number.
         string s="";
         if(num==0) return "0";
         bool b=num<0;
