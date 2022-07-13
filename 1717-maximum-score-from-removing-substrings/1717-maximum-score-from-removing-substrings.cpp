c2a8dc643963d019db882f676940e76359f60877
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-14 02:25:56 +0530

    Time: 192 ms (47.08%), Space: 21.1 MB (72.40%) - LeetHub

--- 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    int helper(string &s,int x, int y, char x1,char y1){
+    int maximumGain(string s, int x, int y) {
+        bool b=x>y;
         int ans=0;
         stack<char> st;
-        for(auto i:s){
+        if(b){
+            for(auto i:s){
                 if(st.empty())
                   {  st.push(i);continue;}
-                else if(st.top()==x1&&i==y1){
+                else if(st.top()=='a'&&i=='b'){
                     st.pop();
                     ans+=x;
                 }
@@ -21,23 +23,41 @@ public:
             for(auto i:s){
                 if(st.empty())
                    { st.push(i);continue;}
-                else if(st.top()==y1&&i==x1){
+                else if(st.top()=='b'&&i=='a'){
                     st.pop();
                     ans+=y;
                 }
                 else
                     st.push(i);
             }
-        return ans;
-    }
-    int maximumGain(string s, int x, int y) {
-        bool b=x>y;
-        if(b){
-            return helper(s,x,y,'a','b');
         }
         else{
-            return helper(s,y,x,'b','a');
+            for(auto i:s){
+                if(st.empty())
+                   { st.push(i);continue;}
+                else if(st.top()=='b'&&i=='a'){
+                    st.pop();
+                    ans+=y;
+                }
+                else
+                    st.push(i);
+            }
+            s="";
+            while(!st.empty()){
+            s+=st.top();
+            st.pop();}
+            reverse(s.begin(),s.end());
+            for(auto i:s){
+                if(st.empty())
+                  {  st.push(i);continue;}
+                else if(st.top()=='a'&&i=='b'){
+                    st.pop();
+                    ans+=x;
+                }
+                else 
+                    st.push(i);
+            }
         }
-        return 0;
+        return ans;
     }
 };
