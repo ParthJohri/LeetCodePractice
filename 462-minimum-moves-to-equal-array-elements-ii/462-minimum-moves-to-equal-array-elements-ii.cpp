0cfe287d2e5164a2b8d54b241228b4b4e1378c84
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-14 22:10:05 +0530

    Time: 14 ms (72.14%), Space: 11.7 MB (5.48%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -3,7 +3,11 @@ public:
     int minMoves2(vector<int>& nums) {
         // sort then find the median of the array nums
         // just find the difference and add it in the ans variable
-        sort(nums.begin(),nums.end(),greater<int>());
+        // sort(nums.begin(),nums.end(),greater<int>());
+        // Using Sort TC: O(NLogN)
+        // Therefore Use Partial Sort or Nth Element to find the Median in O(N) Time
+        auto middle=nums.begin()+nums.size()/2;
+        nth_element(nums.begin(),middle,nums.end());
         int mdpos=nums.size()&1?(nums.size()-1)/2:nums.size()/2;
         int ans=0;
         for(auto i:nums){
