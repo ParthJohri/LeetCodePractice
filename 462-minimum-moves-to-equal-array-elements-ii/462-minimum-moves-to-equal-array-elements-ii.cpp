af5fc944baaa90edf1233ad26a87ce96fac2e719
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-14 21:57:32 +0530

    Time: 21 ms (30.11%), Space: 11.8 MB (5.48%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        int mdpos=nums.size()&1?(nums.size()-1)/2:nums.size()/2;
+        int ans=0;
+        cout<<nums[mdpos]<<endl;
+        for(auto i:nums){
+            ans+=abs(i-nums[mdpos]);
+        }
+        return ans;
+    }
+};
