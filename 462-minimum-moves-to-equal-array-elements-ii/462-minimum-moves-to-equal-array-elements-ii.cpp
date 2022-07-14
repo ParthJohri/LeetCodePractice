22d5ae02b9bf75fee9d548aea8ada5e02ef094c4
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-14 21:58:36 +0530

    Time: 21 ms (30.11%), Space: 11.8 MB (5.48%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        // sort then find the median of the array nums
+        // just find the difference and add it in the ans variable
         sort(nums.begin(),nums.end(),greater<int>());
         int mdpos=nums.size()&1?(nums.size()-1)/2:nums.size()/2;
         int ans=0;
