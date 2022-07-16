0c90713d374f388de52fa50cf980d0b7a4ce1110
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-17 03:04:52 +0530

    Time: 6 ms (55.92%), Space: 13.5 MB (43.37%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -11,33 +11,36 @@
  */
 class Solution {
 public:
+    int sum=0;
+    
     bool leaf(TreeNode* root){
     	return root->left==NULL && root->right==NULL;
     }
-    // int sumOfLeftLeaves(TreeNode* root) {
-    //     if(root==NULL) return 0;
-    //     sumOfLeftLeaves(root->left);
-    //     if(leaf(root))
-    //         sum+=root->val;
-    //     sumOfLeftLeaves(root->right);
-    //     return sum;
-    // }
-    int sumOfLeftLeaves(TreeNode* root){
-        queue<TreeNode*> q;
-        q.push(root);
-        int sum=0;
-        while(!q.empty()){
-            TreeNode* t=q.front();
-            q.pop();
-            if(t->left) {
-                q.push(t->left);
-                if(leaf(t->left))
-                sum+=t->left->val;
-            }
-            if(t->right){
-                q.push(t->right);
-            }
-        }
+    int sumOfLeftLeaves(TreeNode* root, bool leftleaf = false) {
+        if(root==NULL) return 0;
+        sumOfLeftLeaves(root->left,true);
+        if(leftleaf && leaf(root)){  
+            sum+=root->val;
+         }
+        sumOfLeftLeaves(root->right,false);
         return sum;
     }
+    // int sumOfLeftLeaves(TreeNode* root){
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     int sum=0;
+    //     while(!q.empty()){
+    //         TreeNode* t=q.front();
+    //         q.pop();
+    //         if(t->left) {
+    //             q.push(t->left);
+    //             if(leaf(t->left))
+    //             sum+=t->left->val;
+    //         }
+    //         if(t->right){
+    //             q.push(t->right);
+    //         }
+    //     }
+    //     return sum;
+    // }
 };
