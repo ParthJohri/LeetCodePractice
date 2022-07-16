e8cfeed68a659522c416130a62994b9aceb44ce3
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-16 18:29:42 +0530

    Time: 5 ms (96.05%), Space: 11.2 MB (96.67%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> v;
-        for(int i=0;i<(int)pow(2,n);i++){
-            v.push_back(i^(i>>1));
+        int pn=(int)pow(2,n);
+        vector<int> v(pn,0);
+        for(int i=0;i<pn;i++){
+            v[i]=i^(i>>1);
         }
         return v;
     }
