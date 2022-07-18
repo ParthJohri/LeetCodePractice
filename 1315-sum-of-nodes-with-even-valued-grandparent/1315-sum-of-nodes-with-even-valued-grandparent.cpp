2e0979c78e95ece8bac7573a294dfdb3d7686a54
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-19 01:03:24 +0530

    Time: 89 ms (27.17%), Space: 41.5 MB (41.35%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     int sum=0;
-    
+    // Using GrandParents
     int helper(TreeNode* root,int p,int gp){
         if(root==NULL) return 0;
         sum+=gp%2==0?root->val:0;
@@ -20,8 +20,34 @@ public:
         helper(root->right,root->val,p);
         return sum;
     }
+    // int sumEvenGrandparent(TreeNode* root) {
+    //     if(root==NULL) return 0;
+    //     return helper(root,1,1);
+    // }
+    
+    // Using GrandChildren
     int sumEvenGrandparent(TreeNode* root) {
         if(root==NULL) return 0;
-        return helper(root,1,1);
+        if(root->val%2==0){
+            if(root->left){
+                if(root->left->left){
+                    sum+=root->left->left->val;
+                }
+                if(root->left->right){
+                    sum+=root->left->right->val;
+                }
+            }
+            if(root->right){
+                if(root->right->left){
+                    sum+=root->right->left->val;
+                }
+                if(root->right->right){
+                    sum+=root->right->right->val;
+                }
+            }
+        }
+        sumEvenGrandparent(root->left);
+        sumEvenGrandparent(root->right);
+        return sum;
     }
 };
