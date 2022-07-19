958f2ffd2b0a951be734b32d36beaa4f8f33bb1e
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-20 04:41:20 +0530

    Time: 222 ms (80.35%), Space: 96.6 MB (55.17%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -4,6 +4,7 @@ public:
         int sum=0,x=1,p=2,rem=0;
         unordered_map<int,int> m;
         m[0]=-1;
+        if(nums.size()<2) return false;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
             if(k!=0){
