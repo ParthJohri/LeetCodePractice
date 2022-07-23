d02e2ec555475a96538dc3b683d849c2ce3c9320
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-23 14:27:10 +0530

    Time: 6 ms (13.12%), Space: 6 MB (26.85%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -9,11 +9,13 @@ public:
         return sum;
     }
     int countEven(int num) {
-        int count=0;
-        for(int i=1;i<=num;i++){
-            if(sum(i)%2==0)
-                count++;
-        }
-        return count;
+        // int count=0;
+        // for(int i=1;i<=num;i++){
+        //     if(sum(i)%2==0)
+        //         count++;
+        // }
+        // return count;
+        int s=sum(num);
+        return s%2==0?num/2:(num-1)/2;
     }
 };
