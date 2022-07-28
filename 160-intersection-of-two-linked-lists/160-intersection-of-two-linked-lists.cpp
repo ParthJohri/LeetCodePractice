e56c9566eb99ab02d066cb89980dc010d3cec389
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-29 02:27:05 +0530

    Time: 90 ms (26.28%), Space: 21.2 MB (5.49%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,18 +8,34 @@
  */
 class Solution {
 public:
+    // 1st Solution TC: O(M*N) SC:O(1)
+    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //     if(headA==NULL || headB==NULL) return NULL;
+    //     ListNode* curr1=headA,*curr2=headB;
+    //     while(curr1!=NULL){
+    //         curr2=headB;
+    //         while(curr2!=NULL){
+    //             if(curr2->next==curr1||curr2==curr1){
+    //                 return curr1;
+    //             }
+    //             curr2=curr2->next;
+    //         }
+    //         curr1=curr1->next;
+    //     }
+    //     return NULL;
+    // }
+    // 2nd Solution TC:O(M+N) SC:O(M)
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if(headA==NULL || headB==NULL) return NULL;
         ListNode* curr1=headA,*curr2=headB;
+        map<ListNode*,int> m;
         while(curr1!=NULL){
-            curr2=headB;
-            while(curr2!=NULL){
-                if(curr2->next==curr1||curr2==curr1){
-                    return curr1;
-                }
-                curr2=curr2->next;
-            }
+            m[curr1]++;
             curr1=curr1->next;
+        }
+        while(curr2!=NULL){
+            if(m[curr2]) return curr2;
+            curr2=curr2->next;
         }
         return NULL;
     }
