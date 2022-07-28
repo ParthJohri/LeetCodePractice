4332df229aa477c5fc2fb9c73e16b5291b109b87
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-29 02:31:40 +0530

    Time: 75 ms (43.76%), Space: 14.6 MB (31.35%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -25,16 +25,49 @@ public:
     //     return NULL;
     // }
     // 2nd Solution TC:O(M+N) SC:O(M)
+    // ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //     if(headA==NULL || headB==NULL) return NULL;
+    //     ListNode* curr1=headA,*curr2=headB;
+    //     map<ListNode*,int> m;
+    //     while(curr1!=NULL){
+    //         m[curr1]++;
+    //         curr1=curr1->next;
+    //     }
+    //     while(curr2!=NULL){
+    //         if(m[curr2]) return curr2;
+    //         curr2=curr2->next;
+    //     }
+    //     return NULL;
+    // }
+    // 3rd Solution TC:O(M+N) SC:O(1)
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         if(headA==NULL || headB==NULL) return NULL;
         ListNode* curr1=headA,*curr2=headB;
-        map<ListNode*,int> m;
+        int lenA=0,lenB=0;
         while(curr1!=NULL){
-            m[curr1]++;
+            lenA++;
             curr1=curr1->next;
         }
         while(curr2!=NULL){
-            if(m[curr2]) return curr2;
+            lenB++;
+            curr2=curr2->next;
+        }
+        int diff=abs(lenA-lenB);
+        curr1=headA,curr2=headB;
+        if(lenA>lenB){
+            while(diff--){
+            curr1=curr1->next;
+            }
+        }
+        else{
+            while(diff--){
+            curr2=curr2->next;
+            }
+        }
+        while(curr1!=NULL && curr2!=NULL){
+            if(curr1==curr2)
+                return curr1;
+            curr1=curr1->next;
             curr2=curr2->next;
         }
         return NULL;
