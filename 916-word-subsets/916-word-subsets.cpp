a54eef1692ca8d5971291fbad4b52f71de936e13
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-30 21:34:47 +0530

    Time: 1873 ms (5.17%), Space: 237.8 MB (5.84%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int> m;
+        string str="";
+        vector<string> v;
+        unordered_map<char,int> n,fn;
+        int x=0;
+        for(auto i:words2){
+            for(auto j:i){
+                m[j]++;
+            }
+            for(char j='a';j<='z';j++){
+                n[j]=max(n[j],m[j]);
+            }
+            m.clear();
+        }
+        for(auto i:n){
+            if(i.second>0){
+                fn[i.first]=i.second;
+                ++x;}
+        }
+        // for(auto i:fn){
+        //     cout<<i.first<<" "<<i.second<<endl;
+        // }
+        unordered_map<char,int> temp;
+        for(auto i:words1){
+            int c=0;
+            for(auto j:i){
+                temp[j]++;
+            }
+            for(auto j:fn){
+                if(temp[j.first]>=j.second)
+                    ++c;
+            }
+            if(c==fn.size())
+                v.push_back(i);
+            temp.clear();
+        }
+        return v;
+    }
+};
