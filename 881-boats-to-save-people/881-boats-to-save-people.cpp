242f20b58114eb4cb2aaac0cddb647ab3797d56b
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-07-31 00:10:54 +0530

    Time: 134 ms (43.70%), Space: 41.9 MB (90.41%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int count=0,i=0,j=people.size()-1;
+        sort(people.begin(),people.end());
+        while(i<=j){
+            if(people[i]+people[j]>limit){
+                j--;
+            }
+            else if(people[i]+people[j]<=limit){
+                i++;
+                j--;
+            }
+            count++;
+        }
+    return count;
+    }
+};
