73c218292fe2f8941041a26ae13a861eb9a2c1fc
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-02 04:19:47 +0530

    Time: 11 ms (92.61%), Space: 21.6 MB (70.37%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -17,13 +17,21 @@ public:
         v.push_back(root->val);
         dfs(root->right,v);
     }
+    bool dfs(TreeNode* root,long min,long max){
+        if(root==NULL) return true;
+        return root->val>min && root->val<max && dfs(root->left,min,root->val) && dfs(root->right,root->val,max);
+    }
     bool isValidBST(TreeNode* root) {
-        vector<int> v;
-        dfs(root,v);
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i]>=v[i+1])
-                return false;
-        }
-        return true;
+        if(root==NULL) return true;
+        // Recursive Stack and min, max variables
+        return dfs(root,LONG_MIN,LONG_MAX);
+        // Inorder + Vector
+        // vector<int> v;
+        // dfs(root,v);
+        // for(int i=0;i<v.size()-1;i++){
+        //     if(v[i]>=v[i+1])
+        //         return false;
+        // }
+        // return true;
     }
 };
