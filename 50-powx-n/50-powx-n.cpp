f18461ffdd1d4703b651f18d381881c920957a2d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-05 03:17:54 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.61%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -4,13 +4,15 @@ public:
         if(n==0) return 1;
         if(n<0)
         {
-            if(n==INT_MIN)
-            {
+            if(n==INT_MIN){
                 n+=2;
             }
             n=-n;
             x=1/x;
         }
-        return (n&1)?myPow(x*x,n/2)*x:myPow(x*x,n/2);        
+        if(n%2!=0)
+            return myPow(x*x,n/2)*x;
+        else
+            return myPow(x*x,n/2);
     }
 };
