3a8f1d427394f30d90c74d852cc47e765f7f527e
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-05 03:18:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     double myPow(double x, int n) {
         if(n==0) return 1;
-        if(n<0)
-        {
+        if(n<0){
             if(n==INT_MIN){
                 n+=2;
             }
