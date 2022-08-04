b3ed1813267d65579fc624cb573410f2eef8d8c4
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-05 02:05:32 +0530

    Time: 35 ms (19.56%), Space: 23.8 MB (75.07%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -19,51 +19,60 @@ public:
     //     root->right=constructBST(mid+1,end,v);
     //     return root;
     // }
-    void dfs(TreeNode* root,vector<int> &v,int low,int high){
-        if(root==NULL) return;
-        dfs(root->left,v,low,high);
-        if(root->val<low || root->val>high)
-        v.push_back(root->val);
-        dfs(root->right,v,low,high);
-    }
-    TreeNode* successor(TreeNode* root){
-        root=root->right;
-        while(root!=NULL && root->left!=NULL){
-            root=root->left;
-        }
-        return root;
-    }
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root==NULL) return NULL;
-        if(root->val>key)
-            root->left=deleteNode(root->left,key);
-        else if(root->val<key)
-            root->right=deleteNode(root->right,key);
-        else{
-            if(root->left==NULL){
-                TreeNode* temp=root->right;
-                // delete(root);
-                return temp;
-            }
-            else if(root->right==NULL){
-                TreeNode* temp=root->left;
-                // delete(root);
-                return temp;
-            }
-            else{
-                TreeNode* temp=successor(root);
-                root->val=temp->val;
-                root->right=deleteNode(root->right,temp->val);
-            }
-        }
-        return root;
-    }
+    // void dfs(TreeNode* root,vector<int> &v,int low,int high){
+    //     if(root==NULL) return;
+    //     dfs(root->left,v,low,high);
+    //     if(root->val<low || root->val>high)
+    //     v.push_back(root->val);
+    //     dfs(root->right,v,low,high);
+    // }
+    // TreeNode* successor(TreeNode* root){
+    //     root=root->right;
+    //     while(root!=NULL && root->left!=NULL){
+    //         root=root->left;
+    //     }
+    //     return root;
+    // }
+    // TreeNode* deleteNode(TreeNode* root, int key) {
+    //     if(root==NULL) return NULL;
+    //     if(root->val>key)
+    //         root->left=deleteNode(root->left,key);
+    //     else if(root->val<key)
+    //         root->right=deleteNode(root->right,key);
+    //     else{
+    //         if(root->left==NULL){
+    //             TreeNode* temp=root->right;
+    //             // delete(root);
+    //             return temp;
+    //         }
+    //         else if(root->right==NULL){
+    //             TreeNode* temp=root->left;
+    //             // delete(root);
+    //             return temp;
+    //         }
+    //         else{
+    //             TreeNode* temp=successor(root);
+    //             root->val=temp->val;
+    //             root->right=deleteNode(root->right,temp->val);
+    //         }
+    //     }
+    //     return root;
+    // }
     TreeNode* trimBST(TreeNode* root, int low, int high) {
-        vector<int> v;
-        dfs(root,v,low,high);
-        for(int i=0;i<v.size();i++){
-            root=deleteNode(root,v[i]);
+        // vector<int> v;
+        // dfs(root,v,low,high);
+        // for(int i=0;i<v.size();i++){
+        //     root=deleteNode(root,v[i]);
+        // }
+        // return root;
+        if(root==NULL) return NULL;
+        if(root->val>=low && root->val<=high){
+            root->left=trimBST(root->left,low,high);
+            root->right=trimBST(root->right,low,high);
+            return root;
         }
-        return root;
+        if(root->val>high)
+            return trimBST(root->left,low,high);
+        return trimBST(root->right,low,high);
     }
 };
