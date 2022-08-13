3831b33eb2b8112bdbc0c9b7fefbf2275be72e0c
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-14 03:50:33 +0530

    Time: 12 ms (45.46%), Space: 7.2 MB (20.08%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,64 +1,30 @@
 class Solution {
 public:
-    // This problem is the best example of how functions 
-    // are in Mathematics, no element matches 2 elements and
-    // no 2 elements have same input
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> m;
-        unordered_map<char,char> n;
-        // First I created a map which stores 
-        // the unique values like example - 1
-        // e - a, g - d
-        for(int i=0;i<s.length();i++)
-        {
-            if(m[s[i]]=='\0')
-            m[s[i]]=t[i];
+        unordered_map<char,int> m,n;
+        unordered_map<char,char> q;
+        unordered_map<int,int> p;
+        for(int i=0;i<s.length();i++){
+            if(q.count(s[i]) && q[s[i]]!=t[i])
+                return false;
+            q[s[i]]=t[i];
         }
-        // This is for the second relation
-        // badc, baba
-        // Here, also I did the same
-        // b-b, a-a (only the first character)
-        for(int i=0;i<t.length();i++)
-        {
-            if(n[t[i]]=='\0')
-            n[t[i]]=s[i];
+        for(auto i:t){
+            n[i]++;
         }
-        // Now I am reforming the string using the map 
-        // values too check if there is any character that
-        // is not following the constraints
-        string str="",nstr="";
-        for(auto i:s)
-        {
-            str+=m[i];
+        for(auto i:s){
+            m[i]++;
         }
-        for(auto i:t)
-        {
-            nstr+=n[i];
+        for(auto i:m){
+            p[i.second]++;
         }
-        // Checking if both the new reformed
-        // strings matches with the input 
-        // If not then there is some characters
-        // repititive 
-        return str==t&&nstr==s;
+        for(auto i:n){
+            p[i.second]--;
+        }
+        for(auto i:p){
+            if(i.second!=0)
+                return false;
+        }
+        return true;
     }
 };
-// Input
-// "egg"
-// "add"
-// "foo"
-// "bar"
-// "paper"
-// "title"
-// "hello"
-// "johri"
-// "badc"
-// "baba"
-// "bbbaaaba"
-// "aaabbbba"
-// Output
-// true
-// false
-// true
-// false
-// false
-// false
