8e8506a5a3812169672adbd05bfebbf505c0c7a2
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-08-23 02:06:04 +0530

    Time: 19 ms (11.66%), Space: 17.3 MB (11.73%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    // void generate(vector<int> &nums, vector<int> temp, set<vector<int>> &s, int start){
-    //     if(start==nums.size()){        
-    //         s.insert(temp);
-    //         return;
-    //     }
-    //     temp.push_back(nums[start]);
-    //     generate(nums,temp,s,start+1);
-    //     temp.pop_back();
-    //     generate(nums,temp,s,start+1);
-    // }
-    
-    void generate(vector<int> &nums, vector<int> temp, set<vector<int>> &s, int start){
-        s.insert(temp);
-        for(int i=start;i<nums.size();i++){
-            temp.push_back(nums[i]);
-            generate(nums,temp,s,i+1);
-            temp.pop_back();
-        }
+    void subset(set<vector<int>>&ans,vector<int> v,vector<int>&nums,int x){
+        if(x==nums.size()) return;
+        v.push_back(nums[x]);
+        ans.insert(v);
+        subset(ans,v,nums,x+1);
+        v.pop_back();
+        ans.insert(v);
+        subset(ans,v,nums,x+1);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> s;
-        vector<vector<int>> v;
         sort(nums.begin(),nums.end());
-        generate(nums,vector<int>(),s,0);
+        subset(s,vector<int>(),nums,0);
+        vector<vector<int>> ans;
         for(auto i:s){
-            v.push_back(i);
+            ans.push_back(i);
         }
-        return v;
+        return ans;
     }
 };
