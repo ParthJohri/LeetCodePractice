b9606247b411f3026e247aa8315a0e414b189932
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-09-02 01:44:14 +0530

    Time: 34 ms (63.95%), Space: 12.1 MB (66.17%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
-    int numOfPairs(vector<string>& nums, string target) {
+    int numOfPairs(vector<string>& nums, string target){
         int count=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size();j++){
-                if(nums[i]+nums[j]==target and j!=i)
-                    count++;
-            }
+        unordered_map<string,int> m;
+        for(auto i:nums){
+            m[i]++;
         }
+        for(int i=0;i<target.size();i++){
+           string s1=target.substr(0,i);
+           string s2=target.substr(i,target.size());
+            if(s1==s2)
+                count+=m[s1]*(m[s2]-1);
+            else 
+                count+=m[s1]*m[s2];
+        }
+        // 7777
+        // 7, 777
+        // 77, 77
+        // 777,7
+        // 7777, 
         return count;
     }
 };
