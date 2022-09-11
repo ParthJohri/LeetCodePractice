d7dbde9036b8222313e000a45a70504af833d83a
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-09-11 16:52:21 +0530

    Time: 95 ms (15.65%), Space: 8.8 MB (23.29%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> shash(26,0);
-        vector<int> phash(26,0);
+        unordered_map<char,int> m,t;
         vector<int> ans;
-        if(p.length()>s.length()||s=="")
-            return ans;
-        for(int i=0;i<p.length();i++)
-            shash[s[i]-'a']++;
-        for(auto i: p)
-            phash[i-'a']++;
-        
-        int left=0,n=p.length(),right=n-1;
-        
-        while(right<s.length())
-        {
-            if(phash==shash)
-                ans.push_back(left);
-            right++;
-            if(right!=s.length())
-                shash[s[right]-'a']++;
-            shash[s[left]-'a']--;
-            left++;
+        int pos=0;
+        if(p.length()>s.length()) return ans;
+        for(auto i='a';i<='z';i++){
+            m[i]=t[i]=0;
+        }
+        for(auto i:p){
+            t[i]++;
+        }
+        for(int i=0;i<p.length();i++){
+            m[s[i]]++;
+        }
+        if(m==t) ans.push_back(pos);
+        for(int i=p.length();i<s.length();i++){
+            m[s[i-p.length()]]--;
+            m[s[i]]++;
+            pos++;
+            if(m==t) ans.push_back(pos);
         }
         return ans;
     }
