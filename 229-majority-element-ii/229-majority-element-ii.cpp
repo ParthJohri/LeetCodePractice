ab1945d91a1df17408bdaa0e47771ec9cc6eff22
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-09-18 03:00:34 +0530

    Time: 35 ms (11.34%), Space: 16.1 MB (16.78%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
+        // TC:O(NlogN) SC:O(1)
         // sort(nums.begin(),nums.end());
         // int x=nums.size()/3,y=x;
         // if(nums.size()==1){
@@ -26,6 +27,7 @@ public:
         //         ans.push_back(nums[nums.size()-1]);
         //     }
         // }
+        // TC:O(N) SC:O(N)
         map<int,int> m;
         for(auto i:nums){
             m[i]++;
