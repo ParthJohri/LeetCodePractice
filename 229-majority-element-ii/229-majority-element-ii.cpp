369d81895181a8915ec983100ac371651f51bd6b
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-09-18 02:59:59 +0530

    Time: 8 ms (97.93%), Space: 15.7 MB (84.32%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        sort(nums.begin(),nums.end());
-        int x=nums.size()/3,y=x;
-        if(nums.size()==1){
-            ans.push_back(nums[0]);
-            return ans;
+        // sort(nums.begin(),nums.end());
+        // int x=nums.size()/3,y=x;
+        // if(nums.size()==1){
+        //     ans.push_back(nums[0]);
+        //     return ans;
+        // }
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         x--;
+        //     }
+        //     else{
+        //         x--;
+        //         if(x<0){
+        //             ans.push_back(nums[i]);
+        //         }
+        //         x=y;
+        //     }
+        // }
+        // if(nums[nums.size()-1]==nums[nums.size()-2] or y==0){
+        //     x--;
+        //     if(x<0){
+        //         ans.push_back(nums[nums.size()-1]);
+        //     }
+        // }
+        map<int,int> m;
+        for(auto i:nums){
+            m[i]++;
         }
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
-                x--;
-            }
-            else{
-                x--;
-                if(x<0){
-                    ans.push_back(nums[i]);
-                }
-                x=y;
-            }
-        }
-        if(nums[nums.size()-1]==nums[nums.size()-2] or y==0){
-            x--;
-            if(x<0){
-                ans.push_back(nums[nums.size()-1]);
+        for(auto i:m){
+            if(i.second>nums.size()/3){
+                ans.push_back(i.first);
             }
         }
         return ans;
