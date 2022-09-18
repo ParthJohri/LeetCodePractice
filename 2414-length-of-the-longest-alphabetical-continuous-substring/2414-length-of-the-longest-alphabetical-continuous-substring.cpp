05a6af05291b865d19dd38a97a2cbea3b24d5971
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-09-18 22:58:46 +0530

    Time: 87 ms (28.57%), Space: 15.2 MB (14.29%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int longestContinuousSubstring(string s) {
-        string check="abcdefghijklmnopqrstuvwxyz";
-        int j=0,count=1,cc=1;
-        while(j<s.length()-1){
-            if(s[j+1]-s[j]==1){
+        int i=0,count=1,cc=1;
+        while(i<s.length()-1){
+            if(s[i+1]-s[i]==1){
                 cc++;
             }
             else{
                 count=max(count,cc);
                 cc=1;
             }
-            j++;
+            i++;
         }
         count=max(count,cc);
         return count;
