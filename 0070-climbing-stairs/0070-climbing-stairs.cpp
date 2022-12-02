ab35d0075f9ec247c14b6433bb00c82684ba3527
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-12-03 00:00:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int dp[10000]={0};
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+    }
+};
