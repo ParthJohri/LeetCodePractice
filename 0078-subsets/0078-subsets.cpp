a80e115b5915546676e66eaa013afedd60fdc907
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2022-12-03 14:36:03 +0530

    Time: 7 ms (37.48%), Space: 15.9 MB (27.02%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void createSubset(vector<int> &nums,int n,vector<vector<int>>&ans,vector<int>v){
+        if(n<0){
+            ans.push_back(v);
+            return;
+        }
+        createSubset(nums,n-1,ans,v);
+        v.push_back(nums[n]);
+        createSubset(nums,n-1,ans,v);
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> v;
+        createSubset(nums,nums.size()-1,ans,v);
+        return ans;
+    }
+};
