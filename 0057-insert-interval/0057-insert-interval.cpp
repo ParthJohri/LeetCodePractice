3958497fcacf55c93f6342fc0190fbc165b99e56
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-01-17 02:24:46 +0530

    Time: 15 ms (88.80%), Space: 18.7 MB (5.18%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans,nans;
+        bool f = true;
+        if(intervals.size()==0) return {newInterval};
+        for(int i=0;i<intervals.size();i++){
+            if(newInterval[1]<intervals[i][0]){
+                ans.push_back(newInterval);
+                ans.push_back(intervals[i]);
+            }
+            else if(intervals[i][1]>=newInterval[0] and f){
+                int mn=min(intervals[i][0],newInterval[0]);
+                int mx=max(intervals[i][1],newInterval[1]);
+                ans.push_back({mn,mx});
+                f=false;
+            }
+            else
+            ans.push_back(intervals[i]);
+        } 
+        if(f) ans.push_back(newInterval);
+        nans.push_back({ans[0][0],ans[0][1]});
+        for(int i=1;i<ans.size();i++){
+            if(nans[nans.size()-1][1]<ans[i][0])
+            nans.push_back(ans[i]);
+            else if(nans[nans.size()-1][1]>=ans[i][0])
+            nans[nans.size()-1][1]=max(nans[nans.size()-1][1],ans[i][1]);
+        }
+        return nans;
+    }
+};
