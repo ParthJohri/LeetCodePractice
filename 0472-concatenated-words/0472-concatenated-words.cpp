99c43bb781f0a6d82b551f38f249ff609747a089
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-01-28 03:54:53 +0530

    Time: 704 ms (17.28%), Space: 56.6 MB (45.54%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    unordered_map<string,bool> dp;
     bool check(string& words,unordered_set<string>&s){
+        if(dp.find(words)!=dp.end()) return dp[words];
        for(int i=0;i<words.length();i++){
                 string pr=words.substr(0,i+1);
                 string su=words.substr(i+1);
                 if(s.find(pr)!=s.end() and s.find(su)!=s.end() or s.find(pr)!=s.end() and check(su,s)){
-                return true;
+                return dp[words]=true;
                 }
         }
-        return false;
+        return dp[words]=false;
     }
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         unordered_set<string> s;
+        dp.clear();
         vector<string> ans;
         for(auto i:words){
             s.insert(i);
