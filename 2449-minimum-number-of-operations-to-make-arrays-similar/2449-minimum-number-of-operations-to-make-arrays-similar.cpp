505dca1c43194ca343b8ecb6fcaf405294da759d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-01-29 03:43:56 +0530

    Time: 244 ms (57.25%), Space: 86.8 MB (18.07%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    long long makeSimilar(vector<int>& num, vector<int>& t) {
-        vector<int> nums,target,nums2,target2;
-        sort(num.begin(),num.end());
-        sort(t.begin(),t.end());
-        for(auto i:num){
-            if(i%2) nums.push_back(i);
-            else nums2.push_back(i);
+    long long makeSimilar(vector<int>& nums, vector<int>& target) {
+        vector<int> evennums,eventarget,oddnums,oddtarget;
+        sort(nums.begin(),nums.end());
+        sort(target.begin(),target.end());
+        for(auto i:nums){
+            if(i%2) oddnums.push_back(i);
+            else evennums.push_back(i);
         }
-        for(auto i:t){
-            if(i%2) target.push_back(i);
-            else target2.push_back(i);
+        for(auto i:target){
+            if(i%2) oddtarget.push_back(i);
+            else eventarget.push_back(i);
         }
         long long ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(target[i]>nums[i])
-            ans+=(target[i]-nums[i])/2;
+        for(int i=0;i<evennums.size();i++){
+            if(eventarget[i]>evennums[i])
+            ans+=(eventarget[i]-evennums[i])/2;
         }
-        for(int i=0;i<nums2.size();i++){
-            if(target2[i]>nums2[i])
-            ans+=(target2[i]-nums2[i])/2;
+        for(int i=0;i<oddnums.size();i++){
+            if(oddtarget[i]>oddnums[i])
+            ans+=(oddtarget[i]-oddnums[i])/2;
         }
         return ans;
     }
