b1138fc95f21c18e7bedc337403885bd62d903ae
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-01-29 03:38:51 +0530

    Time: 243 ms (58.01%), Space: 86.8 MB (18.07%) - LeetHub

--- 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -14,21 +14,13 @@ public:
         sort(target.begin(),target.end());
         sort(nums2.begin(),nums2.end());
         sort(target2.begin(),target2.end());
-        long long ans=0,s=0,ans2=0,s2=0;
+        long long ans=0,ans2=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<target[i])
-                ans+=(target[i]-nums[i])/2;
-            else
-                s+=(nums[i]-target[i])/2;
+            ans+=abs(target[i]-nums[i])/2;
         }
-        ans=ans+s;
         for(int i=0;i<nums2.size();i++){
-            if(nums2[i]<target2[i])
-                ans2+=(target2[i]-nums2[i])/2;
-            else
-                s2+=(nums2[i]-target2[i])/2;
+            ans2+=abs(target2[i]-nums2[i])/2;
         }
-        ans2=ans2+s2;
         return (ans+ans2)/2;
     }
 };
