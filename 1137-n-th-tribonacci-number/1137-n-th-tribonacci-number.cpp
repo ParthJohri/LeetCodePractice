06d11abf89829e55b99ee133fe26b4605a6c562c
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-01-31 00:44:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (94.16%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
-    int fib(vector<int> &dp,int n) {
-        if(n==0) return 0;
-        if(n==1||n==2) return 1;
-        if(n==3) return 2;
-        if(n==4) return 4;
-        if(dp[n]!=0) return dp[n];
-        dp[n]=fib(dp,n-1)+fib(dp,n-2)+fib(dp,n-3);
-        return dp[n];
-    }
     int tribonacci(int n) {
-        vector<int> dp(n+5,0);
-        return fib(dp,n); // Recusive DP With Memoization
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=1;
-        dp[3]=2;
-        dp[4]=4;
-        for(int i=5;i<=n;i++) // Iterative DP With Memoization
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        return dp[n];
+        int fb[n+5];
+        fb[0]=0;
+        fb[1]=1;
+        fb[2]=1;
+        for(int i=3;i<=n;i++){
+            fb[i]=fb[i-1]+fb[i-2]+fb[i-3];
+
+        }
+        return fb[n];
     }
 };
