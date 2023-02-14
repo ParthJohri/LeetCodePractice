b24184b71416c268d9b0235abc1abb7a2f6d674a
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-02-15 02:06:49 +0530

    Time: 2 ms (73.98%), Space: 6.3 MB (61.45%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+       int i=a.length()-1,j=b.length()-1,c=0;
+        string ans="";
+        while(i>=0 || j>=0 || c==1){
+            c+=i>=0?a[i]-'0':0;
+            c+=j>=0?b[j]-'0':0;
+            ans+=char(c%2+'0');
+            c/=2;
+            i--;
+            j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
