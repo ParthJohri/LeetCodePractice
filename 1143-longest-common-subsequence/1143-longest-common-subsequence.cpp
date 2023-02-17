18e005ce02c19017f8d3dde58594be5f04ceb097
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-02-18 00:45:27 +0530

    Time: 152 ms (5.01%), Space: 19.5 MB (12.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
-    int longCommonSub(string &s,string &t,int m,int n, vector<vector<int>> &dp) {
-        // Recursive Solution : No Memo - TLE O(2^N)
-        // Recurive Solution With Memo : O(M*N)
-        if(m==0||n==0) return 0;
-        if(dp[m][n]!=-1) return dp[m][n];
-        if(s[m-1]==t[n-1])
-            dp[m][n]=1+longCommonSub(s,t,m-1,n-1,dp);
-        else
-            dp[m][n]=max(longCommonSub(s,t,m-1,n,dp),longCommonSub(s,t,m,n-1,dp));
-        return dp[m][n];
-    }
-    int longestCommonSubsequence(string s, string t) {
-        int m=s.length(),n=t.length();
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));        
-        // return longCommonSub(s,t,m,n,dp);
-        // Iterative Tabulation : O(M*N) Faster than Recursive solution With Memo because of Function Calls in the Stack
-        for(int i=0;i<=m;i++)
-            dp[i][0]=0;
-        for(int j=0;j<=n;j++)
-            dp[0][j]=0;
-        for(int i=1;i<=m;i++)
-        {
-            for(int j=1;j<=n;j++)
-            {
-                if(s[i-1]==t[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[m][n];
+    int ans(string& a, string& b, int& n, int& m, int i, int j,vector<vector<int>>&dp){
+        if(i>=n or j>=m) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int an=0;
+        if(a[i]==b[j]) an=1+ans(a,b,n,m,i+1,j+1,dp);
+        return dp[i][j]=max({an, ans(a,b,n,m,i+1,j,dp), ans(a,b,n,m,i,j+1,dp)});
+     }
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.length(),m=text2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return ans(text1,text2,n,m,0,0,dp);
     }
 };
