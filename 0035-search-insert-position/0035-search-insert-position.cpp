93110883f7895c64171b2cd6365211fbba619c6c
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-02-21 01:46:47 +0530

    Time: 10 ms (22.18%), Space: 9.7 MB (31.01%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+        if(target>nums[end])
+            return end+1;
+        while(start<end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]>target)
+                end=mid;
+            else
+                start=mid+1;
+        }
+        return end;
+    }
+};
