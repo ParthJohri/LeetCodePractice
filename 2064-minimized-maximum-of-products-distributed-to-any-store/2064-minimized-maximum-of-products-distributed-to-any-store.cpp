bfe1548c6c43a85d2a93bc60041c8638d1f7477d
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-03-06 12:46:33 +0530

    Time: 359 ms (17.12%), Space: 84.5 MB (55.95%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int fn(vector<int>&nums,int mid){
-        int sum=0;
+    long long int fn(vector<int>&nums,long long int mid){
+        long long int sum=0;
         for(int i=0;i<nums.size();i++){
             sum+=(nums[i]+mid-1)/mid;
         }
         return sum;
     }
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        int left=1,right=100000,ans=0;
+    long long int minimizedMaximum(int n, vector<int>& quantities) {
+        long long int left=1,right=INT_MAX,ans=0;
         while(left<=right){
-            int mid=left+(right-left)/2;
+            long long int mid=left+(right-left)/2;
             if(fn(quantities,mid)<=n){
                 ans=mid;
                 right=mid-1;
