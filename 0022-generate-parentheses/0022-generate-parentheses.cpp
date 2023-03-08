46ff8768a9a95bfb9b68e93d1a5d5590d4fd9cad
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-03-08 15:13:43 +0530

    Time: 4 ms (64.59%), Space: 13.5 MB (69.11%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    void recur(string s,int leftparanthesis,vector<string>&ans,int rightparanthesis){
-        if(leftparanthesis==0 and rightparanthesis==0){
+    void recur(string s,int n,int leftparanthesis,vector<string>&ans,int rightparanthesis){
+        if(s.length()==2*n){
             ans.push_back(s);
             return;
         }
-        if(rightparanthesis>0) recur(s+")",leftparanthesis,ans,rightparanthesis-1);
-        if(leftparanthesis>0) recur(s+"(",leftparanthesis-1,ans,rightparanthesis+1);
+        if(leftparanthesis<n) recur(s+"(",n,leftparanthesis+1,ans,rightparanthesis);
+        if(rightparanthesis<leftparanthesis) recur(s+")",n,leftparanthesis,ans,rightparanthesis+1);
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        recur("",n,ans,0);
+        recur("",n,0,ans,0);
         return ans;
     }
 };
