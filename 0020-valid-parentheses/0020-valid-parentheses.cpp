0685e093406106bdeee19f26758e78c05a29e9f4
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-04-10 12:20:15 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string st) {
+        stack<char> s;
+        bool b=true;
+        if(st.length()&1)
+            b=false;
+        for(auto i:st)
+        {
+            if(i=='('||i=='{'||i=='[')
+                s.push(i);
+            else if(!s.empty())
+            {
+                switch(i)
+                    {
+                    case '}':if(s.top()=='{')
+                                s.pop();
+                              else
+                              return false;
+                                break;
+                    case ')': if(s.top()=='(')
+                                s.pop();
+                              else
+                              return false;
+                                break;
+                    case ']': if(s.top()=='[')
+                                s.pop();
+                              else
+                                 return false;
+                                break;
+                }
+            }
+            else
+                b=false;
+        }
+        if(s.size()>0)
+            b=false;
+        return b;
+    }
+};
