6a8fac12d2334a327e099aa927d1ecfdff1d5890
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-04-13 02:28:48 +0530

    Time: 3 ms (41.24%), Space: 6 MB (76.51%) - LeetHub

---
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string str=to_string(n);
+        int position=-1,smallestright;
+        for(int i=str.length()-2;i>=0;i--){
+            if(str[i]<str[i+1]) {
+                position=i;
+                break;
+            }
+        }
+        if(position==-1) return -1;
+        smallestright=position;
+        for(int i=position;i<str.length();i++){
+            if(str[position]<str[i]){
+                smallestright=i;
+            }
+        }
+        swap(str[position],str[smallestright]);
+        sort(str.begin()+position+1,str.end());
+        long ans=stol(str);
+        return ans>INT_MAX?-1:ans;
+    }
+};
