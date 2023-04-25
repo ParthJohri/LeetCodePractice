cb055b62041d83952b8fc37cc8f0726824fb05f5
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-04-25 22:24:35 +0530

    Time: 76 ms (88.14%), Space: 35.3 MB (97.22%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,21 +1,27 @@
 class SmallestInfiniteSet {
 public:
-    priority_queue<int,vector<int>,greater<int>> s;
-    unordered_map<int,int> m;
+    int top=1;
+    priority_queue<int,vector<int>,greater<int>> p;
+    map<int,int> m;
     SmallestInfiniteSet() {
-        for(int i=1;i<=1000;i++)
-            {s.push(i);m[i]++;}
+        
     }
     
     int popSmallest() {
-        int p=s.top();
-        s.pop();
-        m[p]--;
-        return p;
+        if(!p.empty() and p.top()<top) {
+            int num=p.top();
+            p.pop();
+            m.erase(num);
+            return num;
+        }
+        return top++;
     }
     
     void addBack(int num) {
-        if(m[num]==0) {s.push(num);m[num]++;}
+        if(top>num and !m.count(num)){
+            p.push(num);
+            m[num]++;
+        }
     }
 };
 
