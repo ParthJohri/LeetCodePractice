c15b33ae951df22a1b19007d5c7c0da28c2591f4
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-04-27 12:26:49 +0530

    Time: 428 ms (5.63%), Space: 163.6 MB (5.63%) - LeetHub

--- 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
+++ 0672-bulb-switcher-ii/0672-bulb-switcher-ii.cpp
@@ -3,31 +3,32 @@ private:
     set<string> s;
 public:
     string flip(int type,string& s){
-        if(type==0){
-            for(int i = 0;i<s.length();i++){
-                if(s[i]=='1') s[i]='0';
-                else s[i]='1';
-            }
-        }
-        else if(type==1){
-            for(int i = 1;i<s.length();i+=2){
-                if(s[i]=='1') s[i]='0';
-                else s[i]='1';
+        int n=s.length();
+        if(type==1){
+            for(int i=0;i<n;i++){
+            if(s[i]=='1') s[i]='0';
+            else s[i]='1';
             }
         }
         else if(type==2){
-            for(int i = 0;i<s.length();i+=2){
-                if(s[i]=='1') s[i]='0';
-                else s[i]='1';
+        for(int i=1; i<n;i+=2){
+            if(s[i]=='1') s[i]='0';
+            else s[i]='1';
+        }}
+        else if(type==3){
+        for(int i=0;i<n;i+=2){
+            if(s[i]=='1') s[i]='0';
+            else s[i]='1';
             }
         }
         else{
-            for(int k = 0;3*k+1<=s.length();k++){
-                int i = 3*k;
-                if(s[i]=='1') s[i]='0';
-                else s[i]='1';
-            }
+           for(int k=0;3*k+1<=n and type==4;k++){
+            int i=3*k;
+            if(s[i]=='1') s[i]='0';
+            else s[i]='1';
+            } 
         }
+        
         return s;
     }
     void recur(string& str,int presses,unordered_map<string,unordered_map<int,int>>&m){
@@ -37,13 +38,13 @@ public:
         }
         if(m.count(str) and m[str].count(presses)) return;
         m[str][presses]=1;
-        string ans=flip(0,str);
-        recur(ans,presses-1,m);
-        ans=flip(1,str);
+        string ans=flip(1,str);
         recur(ans,presses-1,m);
         ans=flip(2,str);
         recur(ans,presses-1,m);
         ans=flip(3,str);
+        recur(ans,presses-1,m);
+        ans=flip(4,str);
         recur(ans,presses-1,m);
     }
     int flipLights(int n, int presses) {
