3439564bc1022c3f05c4bff697c62655f3e22643
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-04-28 13:49:38 +0530

    Time: 342 ms (23.49%), Space: 10.6 MB (46.98%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    int parent[100000],size[100000];
+
+    void make(int v){
+        parent[v]=v;
+        size[v]=1;
+    }
+
+    int find(int v){
+        if(parent[v]==v) return v;
+        return parent[v]=find(parent[v]);
+    }
+
+    void Union(int a,int b){
+        a=find(parent[a]);
+        b=find(parent[b]);
+        if(a!=b){
+            if(size[a]<size[b]){
+            swap(a,b);
+            }
+            parent[b]=a;
+            size[a]+=size[b];
+        }
+    }
     void dfs(int i,vector<string>&strs,vector<bool>&vis){
         vis[i]=true;
         for(int j=0;j<strs.size();j++){
@@ -16,13 +39,25 @@ public:
         return count==2 or count==0;
     }
     int numSimilarGroups(vector<string>& strs) {
-        int groups=0;
+        int groups=0,n=strs.size(),ans=0;
         vector<bool> vis(strs.size(),false);
         for(int i=0;i<strs.size();i++){
             if(vis[i]) continue;
             dfs(i,strs,vis);
             groups++;
         }
-        return groups;
+        for(int i=1;i<=n;i++){
+            make(i);
+        }
+        for(int i=0;i<strs.size();i++){
+            for(int j=i+1;j<strs.size();j++){
+                if(anagram(strs[i],strs[j]))
+                    Union(i+1,j+1);
+            }
+        }
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i) ans++;
+        }
+        return ans;
     }
 };
