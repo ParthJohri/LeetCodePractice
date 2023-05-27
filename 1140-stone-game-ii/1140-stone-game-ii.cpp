7ce3f5c78363f4bb1906d1f77a320ea9c1f397eb
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-05-27 23:52:56 +0530

    Time: 163 ms (22.19%), Space: 37.9 MB (10.44%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int n;
-    int dp[105][105];
-    int dfs(vector<int>&p, int i,int m)
-    {
-        if(i==n) return 0;
-        if(dp[i][m]!=-1) return dp[i][m];
-        int ans=INT_MIN,sum=0;
-        for(int j=i;j<n && j-i+1<=2*m;j++)
-        {
+    int recur(vector<vector<vector<int>>> &dp,vector<int>& p,int i,int m,int pl){
+        if(i>=p.size()) return 0;
+        if(dp[i][m][pl]!=-1) return dp[i][m][pl];
+        int count=0,sum=0,result=(pl==0?INT_MIN:INT_MAX);
+        for(int j=i;j<p.size();j++){
             sum+=p[j];
-            ans=max(ans,sum-dfs(p,j+1,max(m,j-i+1)));
+            count++;
+            if(count>2*m) break;
+            if(pl==0)
+            result=max(result,sum+recur(dp,p,j+1,max(m,count),1));
+            else
+            result=min(result,recur(dp,p,j+1,max(m,count),0));
         }
-        return dp[i][m]=ans;
+        return dp[i][m][pl]=result;
     }
-    int stoneGameII(vector<int>& piles) {
-        n=piles.size();
-        memset(dp,-1,sizeof(dp));
-        int sum= accumulate(piles.begin(),piles.end(),0);
-        //cout<<sum<<" "<<dfs(piles,0,1);
-        return (sum+dfs(piles,0,1))/2;
+    int stoneGameII(vector<int>& p) {
+        int n=p.size();
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(101,vector<int>(2,-1)));
+        return recur(dp,p,0,1,0);
     }
 };
