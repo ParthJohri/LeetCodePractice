5f215f016f5994ddb3ca8cdb4e5a6fbd844e0c94
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-06-01 15:52:14 +0530

    Time: 39 ms (7.74%), Space: 16.2 MB (5.60%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&G, vector<int>& colors, int i, int color) {
-        colors[i] = color;
-        for (int j = 0; j < G[i].size(); j++) {
-            int k = G[i][j]; // adjacent node
-            if (colors[k] == -color) continue;
-            if (colors[k] == color || !dfs(G, colors, k, -color)) return false;
+    bool bfs(vector<int> *adj,int start,vector<int>&color){
+        queue<int> q;
+        q.push(start);
+        while(!q.empty()){
+            int sz=q.size();
+            while(sz--){
+                int p=q.front();
+                q.pop();
+                for(auto node:adj[p]){
+                    if(color[node]==-1){
+                        color[node]=!color[p];
+                        q.push(node);
+                    }
+                    if(color[node]==color[p]) return false;
+                }
+            }
         }
         return true;
     }
-    bool isBipartite(vector<vector<int>>& G) {
-        int n = G.size();
-        vector<int> colors(n,false);
-        for (int i = 0; i < n; i++) {
-            if (colors[i] == 0 && !dfs(G, colors, i, 1)) 
-                return false;            
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int> adj[n];
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){
+            for(auto j:graph[i])
+            adj[i].push_back(j);
+        }
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                color[i]=0;
+                bool flag=bfs(adj,i,color);
+                if(flag==false) return false;
+            }
         }
         return true;
     }
