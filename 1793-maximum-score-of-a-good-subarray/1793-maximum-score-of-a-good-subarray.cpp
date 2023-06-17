b2fe5872fbe6beeb45b11ddbf3b42422f32a4b66
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-06-18 03:03:00 +0530

    Time: 333 ms (21.99%), Space: 108.8 MB (7.67%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> stackPreviousSmaller(vector<int>&heights){
+        int n=heights.size();
+        vector<int>prevSmaller(n,-1);
+        stack<int> s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() and heights[s.top()]>=heights[i]){
+                    s.pop();
+            }
+            if(s.empty()){
+                prevSmaller[i]=-1;//start of array
+            }
+            else{
+                prevSmaller[i]=s.top();
+            }
+            s.push(i);//pushing index of elements
+        }
+        return prevSmaller;
+    }
+    vector<int> stackNextSmaller(vector<int>&heights){
+        int n=heights.size();
+        vector<int> nextSmaller(n,-1);
+        stack<int> s;
+        for(int i=n-1;i>=0;i--){
+            while(!s.empty() and heights[s.top()]>=heights[i]){
+                s.pop();
+            }
+            if(s.empty()){
+                nextSmaller[i]=n;//end of array
+            }
+            else{
+                nextSmaller[i]=s.top();
+            }
+            s.push(i);//pushing index of elements
+        }
+        return nextSmaller;
+    }
+    int largestRectangleArea(vector<int>& heights,int k) {
+        int n=heights.size(),ans=0,calculatedValue=0;
+        vector<int> prevSmaller(n,-1),nextSmaller(n,-1);
+        prevSmaller=stackPreviousSmaller(heights);
+        nextSmaller=stackNextSmaller(heights);
+        for(int i=0;i<n;i++){
+            if(prevSmaller[i]<k and nextSmaller[i]>k)
+            calculatedValue=(nextSmaller[i]-prevSmaller[i]-1)*heights[i];
+            ans=max(ans,calculatedValue);
+        }
+        return ans;
+    }
+    int maximumScore(vector<int>& nums, int k) {
+        int ans=largestRectangleArea(nums,k);
+        return ans;
+    }
+};
