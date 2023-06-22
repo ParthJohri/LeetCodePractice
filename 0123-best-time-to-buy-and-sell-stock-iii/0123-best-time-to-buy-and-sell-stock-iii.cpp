649fc6696a6d2541fa47d1b1ad4a663570f5bb15
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-06-23 00:37:38 +0530

    Time: 987 ms (5.01%), Space: 245.7 MB (5.22%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-    int recur(int n, vector<int>& prices,vector<vector<vector<int>>>&dp,int s,int count){
-        if(n>=prices.size()) return 0;
-        if(dp[n][s][count]!=-1) return dp[n][s][count];
-        int ans=0;
-        if(s==0) {
-            if(count==2){
-                ans=recur(n+1,prices,dp,s,count);
-            }
-            else{
-                ans=max(recur(n+1,prices,dp,s,count),-prices[n]+recur(n+1,prices,dp,s+1,count));
-            }
+    int recur(vector<int>&prices,int index,int k,bool flag,vector<vector<vector<int>>>&dp){
+        if(index>=prices.size() or k==0) return 0;
+        if(dp[index][flag][k]!=-1) return dp[index][flag][k];
+        int profit=0;
+        if(flag){
+            int buy=recur(prices,index+1,k,false,dp)-prices[index];
+            int notbuy=recur(prices,index+1,k,true,dp);
+            profit=max({profit,buy,notbuy});
         }
-        else if(s==1) {
-            if(count==2){
-                ans=recur(n+1,prices,dp,s,count);
-            }
-            else{
-                ans=max(recur(n+1,prices,dp,s,count),+prices[n]+recur(n+1,prices,dp,s-1,count+1));
-            }
+        else{
+            int sell=prices[index]+recur(prices,index+1,k-1,true,dp);
+            int notsell=recur(prices,index+1,k,false,dp);
+            profit=max({profit,sell,notsell});
         }
-        return dp[n][s][count]=ans;
+        return dp[index][flag][k]=profit;
     }
     int maxProfit(vector<int>& prices) {
-        int n=prices.size(),s=0,count=0;
-        if(n==1) return 0;
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        // dp[i][1] - stock in hand
-        // dp[i][0] -  stock not in hand
-        return recur(0,prices,dp,s,count);
+        int k=2,n=prices.size();
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,-1)));
+        return recur(prices,0,2,true,dp);
     }
 };
