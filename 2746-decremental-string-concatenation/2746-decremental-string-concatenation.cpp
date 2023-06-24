c47023441ad9c849fac596758cad5c4b5dac4a00
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-06-25 01:35:42 +0530

    Time: 380 ms (55.56%), Space: 236.2 MB (11.11%) - LeetHub

--- 2746-decremental-string-concatenation/2746-decremental-string-concatenation.cpp
+++ 2746-decremental-string-concatenation/2746-decremental-string-concatenation.cpp
@@ -14,35 +14,33 @@ public:
    //      }
    //      return ans;
    //  }
-    int dp[1501][27][27];
-    int recur(vector<string>&words,int index,int start,int end){
+    int recur(vector<string>&words,int index,int start,int end,vector<vector<vector<int>>>&dp){
         if(index>=words.size()) return 0;
         if(dp[index][start-'a'][end-'a']!=-1) return dp[index][start-'a'][end-'a'];
         int mn=INT_MIN,n=words[index].length(),lastChar=words[index][n-1];
         if(words[index][n-1]==start){
-            mn=max(mn,1+recur(words,index+1,words[index][0],end));
+            mn=max(mn,1+recur(words,index+1,words[index][0],end,dp));
         }
         else{
-            mn=max(mn,0+recur(words,index+1,words[index][0],end));
+            mn=max(mn,0+recur(words,index+1,words[index][0],end,dp));
         }
         if(words[index][0]==end){
-            mn=max(mn,1+recur(words,index+1,start,lastChar));
+            mn=max(mn,1+recur(words,index+1,start,lastChar,dp));
         }
         else{
-            mn=max(mn,0+recur(words,index+1,start,lastChar));
+            mn=max(mn,0+recur(words,index+1,start,lastChar,dp));
         }
         return dp[index][start-'a'][end-'a']=mn;
     }
     int minimizeConcatenatedLength(vector<string>& words) {
         int n=words.size();
-        memset(dp,-1,sizeof dp);
         if(words.size()==1) return words[0].length();
-
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(27,vector<int>(27,-1)));
         int count=0;
         for(auto i:words){
             count+=i.size();
         }
-        int ans=recur(words,1,words[0][0],words[0][words[0].length()-1]);
+        int ans=recur(words,1,words[0][0],words[0][words[0].length()-1],dp);
         return count-ans;
     }
 };
