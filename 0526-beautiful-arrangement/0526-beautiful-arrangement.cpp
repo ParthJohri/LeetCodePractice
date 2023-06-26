6a3fe838b565cbcb46fd57ebaa44730956084676
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-06-26 22:03:10 +0530

    Time: 72 ms (72.56%), Space: 6 MB (85.54%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool check[16];
+    int seen=0;
     int ans=0;
     void recur(vector<int>& v,int n,int index){
         if(index>n){
@@ -8,18 +8,19 @@ public:
             return;
         }
         for(int i=1;i<=n;i++){
-            if(!check[i] and (i%index==0 or index%i==0)){
-                v.push_back(i);
-                check[i]=true;
+            int bit=(1<<i);
+            if(!(seen&bit)and (i%index==0 or index%i==0)){
+                seen^=(1<<i);
                 recur(v,n,index+1);
-                v.pop_back();
-                check[i]=false;
+                seen^=(1<<i);
             }
         }
     }
     int countArrangement(int n) {
         vector<int> v;
         recur(v,n,1);
+        for(auto i:v)cout<<i<<" ";
+        cout<<endl;
         return ans;
     }
 };
