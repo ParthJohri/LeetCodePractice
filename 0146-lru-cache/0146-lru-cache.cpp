75373f5a3c985101b20c7a7aee254d4b696de016
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-07-19 05:20:37 +0530

    Time: 597 ms (7.03%), Space: 181.6 MB (9.18%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,39 +1,62 @@
+class Node{
+        public:
+         int key;
+         int val;
+         Node* next;
+         Node* prev;
+        public:
+         Node(int k,int v){
+             this->key=k;
+             this->val=v;
+             next=NULL;
+             prev=NULL;
+         }
+    };
 class LRUCache {
 public:
-    list<int> dll;
-    map<int,pair<list<int>:: iterator,int>> m;
-    int n;
+    map<int,Node*> m;
+    int sz;
+    Node* head=new Node(0,0);
+    Node* tail=new Node(0,0);
     LRUCache(int capacity) {
-        n=capacity;
+        head->next=tail;
+        tail->prev=head;
+        this->sz=capacity;
     }
-    
+    void insert(Node* curr){
+        curr->next=head->next;
+        head->next->prev=curr;
+        curr->prev=head;
+        head->next=curr;
+        sz--;
+        m[curr->key]=curr;
+    }
+    void remove(Node* curr){
+        m.erase(curr->key);
+        curr->next->prev=curr->prev;
+        curr->prev->next=curr->next;
+        sz++;
+    }
     int get(int key) {
-        if(m.find(key)!=m.end()){
-            dll.erase(m[key].first);
-            dll.push_front(key);
-            m[key].first=dll.begin();
-            return m[key].second;
-        }
-        return -1;
-    }
-    
-    void put(int key, int value) {
-        if(m.find(key)!=m.end()){
-            m[key].second=value;
-            dll.erase(m[key].first);
-            dll.push_front(key);
-            m[key].first=dll.begin();
+        if(m.count(key)){
+            Node* n=m[key];
+            remove(m[key]);
+            insert(n);
+            return n->val;
         }
         else{
-            dll.push_front(key);
-            m[key]={dll.begin(),value};
-            n--;
+            return -1;
         }
-        if(n<0){
-            m.erase(dll.back());
-            dll.pop_back();
-            n++;
+    }
+    void put(int key, int value) {
+        if(m.count(key)){
+            remove(m[key]);
         }
+        if(sz==0){
+            remove(tail->prev);
+        }
+        Node* n=new Node(key,value);
+        insert(n);
     }
 };
 
