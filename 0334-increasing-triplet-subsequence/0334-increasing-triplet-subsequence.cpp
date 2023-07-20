88a5027c648dd320ca3f53e8d54506192c78c1c5
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-07-21 03:59:14 +0530

    Time: 136 ms (96.41%), Space: 111.5 MB (95.55%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
         int a=INT_MAX,b=INT_MAX;
+        // Pta kya ho rha tha na dekho pehle a dhundo woh toh tum sahi jaa rhe the
+        // ki sabse chota hoga mil gya a mein
+        // ab a se bada but second minimum woh b hai 
+        // woh bhi mil gya toh kyun bethe ho khali
+        // bajao tali kyunki mil gye tumko triplet
+        // jo bhi yeh padh rha hai bhai mere ko bhi smjh nhi aarha ab toh kyun likh rha hoon mein :)
         for(auto i:nums){
             if(a>=i){
                 a=i;
