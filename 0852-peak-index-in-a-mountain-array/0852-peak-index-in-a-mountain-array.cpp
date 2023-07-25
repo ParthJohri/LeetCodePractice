e8abac50ab8767dbf043f3249cc7dedbe7f29ce7
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-07-26 00:27:31 +0530

    Time: 132 ms (35.53%), Space: 59.5 MB (88.25%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,14 +4,17 @@ public:
         int start=0;
         int end=arr.size()-1;
         int mid=0;
+        int ans=0;
         while(start<end)
         {
             mid=start+(end-start)/2;
             if(arr[mid]>arr[mid+1])
-                end=mid;
+                { ans = mid;
+                  end = mid;
+                }
             else
                 start=mid+1;
         }
-        return start;
+        return ans;
     }
 };
