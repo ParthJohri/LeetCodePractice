8061eaaab45b6e1363d8b6fdc1e984098dbadedd
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-08-11 02:30:22 +0530

    Time: 9 ms (25.73%), Space: 14 MB (51.17%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,12 +5,15 @@ class Solution
         {
             int n = nums.size() - 1;
             int start = 0, end = n;
+            // Worst Time Complexity : O(N) 
             while (start <= end)
             {
+                // To remove duplicates from starting of the array
                 while (start < n and start <= end and nums[start] == nums[start + 1])
                 {
                     start++;
                 }
+                // To remove duplicates from ending of the array
                 while (end > 0 and start <= end and nums[end] == nums[end - 1])
                 {
                     end--;
