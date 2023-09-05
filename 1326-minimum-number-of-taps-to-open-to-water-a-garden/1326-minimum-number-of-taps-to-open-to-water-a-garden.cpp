22e3bfe4aa5beb2c55e322d187a93937f5717f52
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-09-05 13:40:09 +0530

    Time: 22 ms (49.82%), Space: 15 MB (29.43%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<vector<int>> lr;
-        int x=0,min=0,max=0,count=0;
-        for(auto i:ranges){
-            lr.push_back({x-i,x+i});
-            x++;
-        }
-        while(max<n){
-            for(auto i:lr){
-                if(min>=i[0] and max<=i[1]){
-                    max=i[1];
-                }
+        vector<long long int> dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<=n;i++){
+            int left_coverage = max(i-ranges[i],0);
+            int right_coverage = min(i+ranges[i],n);
+            for(int j=left_coverage;j<=right_coverage;j++){
+                dp[j]=min(dp[j],dp[left_coverage]+1);
             }
-            if(min==max) return -1;
-            min=max;
-            count++;
         }
-        return count;
+        return dp[n]==INT_MAX?-1:dp[n];
     }
 };
