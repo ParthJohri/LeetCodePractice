564936a8add5bda56dd5b520ed3754f943b8a1e8
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-10-12 13:04:58 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (43.62%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1095-find-in-mountain-array.cpp
@@ -0,0 +1,75 @@
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class MountainArray {
+ *   public:
+ *     int get(int index);
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int ascending(int target, MountainArray &mountainArr,int m){
+        int n=mountainArr.length();
+        int start=0,end=m,ans=-1;
+        while(start<=end and start>=0 and end<=m){
+            int mid=start+(end-start)/2;
+            int ele=mountainArr.get(mid);
+            if(ele==target){
+                ans=mid;
+                end=mid-1;
+            }
+            else if(ele>target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+    int descending(int target, MountainArray &mountainArr,int m){
+        int n=mountainArr.length();
+        int start=m,end=n-1,ans=-1;
+        while(start<=end and start<n and end>=m){
+            int mid=start+(end-start)/2;
+            int ele=mountainArr.get(mid);
+            if(ele==target){
+                ans=mid;
+                start=mid+1;
+            }
+            else if(ele>target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+    int findInMountainArray(int target, MountainArray &mountainArr) {
+        int n=mountainArr.length();
+        int start=0,end=n-1,ans=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int ele=mountainArr.get(mid);
+            cout<<mid<<endl;
+                if(mid!=0 and mid!=n-1 and mountainArr.get(mid+1)<ele and ele>=mountainArr.get(mid-1)){
+                    int x=ascending(target,mountainArr,mid);
+                    int y=descending(target,mountainArr,mid);
+                    if(x!=-1 and y!=-1) return min(x,y);
+                    ans=max(x,y);
+                    return ans;
+                }
+                else if(mid==n-1) {
+                    end=mid-1;
+                }
+                else if(mid==0 or mountainArr.get(mid+1)>=ele and ele>=mountainArr.get(mid-1)){
+                    start=mid+1;
+                }
+                else end=mid-1;
+        }
+        return ans;
+    }
+};
