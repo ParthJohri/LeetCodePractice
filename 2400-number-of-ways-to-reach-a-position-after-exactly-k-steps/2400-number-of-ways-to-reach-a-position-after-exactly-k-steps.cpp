de31b274851df720bc4c98d7ea45fc51595c5ee5
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-10-15 11:50:52 +0530

    Time: 2714 ms (5.29%), Space: 254.5 MB (24.70%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    map<int,map<int,int>> dp;
+    int mod=1e9+7;
+    int recur(int k,int t, int i){
+        if(k<0  or (abs(i)+k)<abs(t)) return 0;
+        if(k==0) return i==t;
+        if(dp[i].count(k)) return dp[i][k];
+        int a=recur(k-1,t,i+1)%mod;
+        int b=recur(k-1,t,i-1)%mod;
+        return dp[i][k]=(a%mod+b%mod)%mod;
+    }
+    int numberOfWays(int startPos, int endPos, int k) {
+        return recur(k,endPos,startPos)%mod;
+    }
+};
