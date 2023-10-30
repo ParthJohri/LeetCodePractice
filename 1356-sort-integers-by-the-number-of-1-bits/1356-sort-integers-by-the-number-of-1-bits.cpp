8eb216ea577f7a7759cc10c15f0e393a07f24ceb
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-10-30 14:00:09 +0530

    Time: 27 ms (5.25%), Space: 12.7 MB (5.36%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
-    int countBits(int n)
-    {
-        int count=0;
-        while(n)
-        {
-            ++count;
-            n=n&(n-1);
-        }
-        return count;
-    }
-    static bool check(pair<int,int> p1, pair<int,int> p2)
-    {
-        if(p1.second!=p2.second) 
-            return p1.second<p2.second; 
-        return p1.first<p2.first;
-    }
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>> m;
-        vector<int> v;
-        int c=0;
-        for(int i=0;i<arr.size();i++)
-        {
-            c=countBits(arr[i]);
-            m.push_back(make_pair(arr[i],c));
+        int n=arr.size();
+        vector<vector<int>> bit(n,vector<int>(2,0));
+        vector<int> ans;
+            for(int in=0;in<n;in++){
+                for(int i=0;i<=31;i++){   
+                if(arr[in]&(1<<i)){
+                    int temp=bit[in][0];
+                    bit[in]={temp+1,arr[in]};
+                }
+            }
         }
-        sort(m.begin(),m.end(),check);
-        for(auto i:m)
-            v.push_back(i.first);
-        return v;
+        sort(bit.begin(),bit.end());
+        for(auto i:bit){
+            ans.push_back(i[1]);
+        }
+        return ans;
     }
 };
