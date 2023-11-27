f8cad18e3ce8e6d400b71e6fff3c2b4ecd53b277
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-11-28 03:13:12 +0530

    Time: 121 ms (50.88%), Space: 95.7 MB (49.93%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
-    long long int dp[100001];
-    long long int recur(int i,string&c, vector<int>&n){
-        if(i>=c.size()-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        long long int ans=INT_MAX;
-        if(c[i]==c[i+1]){
-            if(n[i]>n[i+1]) swap(n[i],n[i+1]);
-            ans=min(n[i],n[i+1])+recur(i+1,c,n);
-        }
-        else ans=recur(i+1,c,n);
-        return dp[i]=ans;
-    }
     int minCost(string colors, vector<int>& neededTime) {
-        memset(dp,-1,sizeof(dp));
-        return recur(0,colors,neededTime);
+        int ans=0;
+        for(int i=1;i<colors.size();i++){
+            if(colors[i]==colors[i-1]){
+                ans+=min(neededTime[i],neededTime[i-1]);
+                neededTime[i]=max(neededTime[i],neededTime[i-1]);
+            }
+        }
+        return ans;
     }
 };
