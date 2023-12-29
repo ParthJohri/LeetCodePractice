46c532387384338b59b17d1fc20f783cc4f13ebe
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2023-12-29 18:47:32 +0530

    Time: 72 ms (30.60%), Space: 10.1 MB (6.26%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     long long int dp[1007][301];
-    long long int recur(vector<int>&job,int i,int d,int v){
+    long long int recur(vector<int>&job,int i,int d){
         if(i==job.size() and d==0){
             return 0;
         }
@@ -10,8 +10,8 @@ public:
         long long int sum=0,ans=INT_MAX;
         for(int j=i;j<job.size();j++){
             sum=max(sum,1ll*job[j]);
-            int a=recur(job,j+1,d-1,0);
-            int b=recur(job,j+1,d,sum);
+            int a=recur(job,j+1,d-1);
+            int b=recur(job,j+1,d);
             int val=min(a,b);
             ans=min(ans,sum+val);
         }
@@ -19,9 +19,9 @@ public:
     }
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n=jobDifficulty.size();
-        // if(n<d) return -1;
+        if(n<d) return -1;
         memset(dp,-1,sizeof(dp));
-        int ans = recur(jobDifficulty,0,d,0);
+        int ans = recur(jobDifficulty,0,d);
         return ans==INT_MAX?-1:ans;
     }
 };
