6bb3f891df938f22297a337d1124e7dbb4af6821
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2024-01-24 22:12:32 +0530

    Time: 101 ms (54.90%), Space: 51.2 MB (16.56%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
+    long long int power(long long int base,long long int exp,int mod){
+        long long int ans=1;
+        while(exp){
+            if(exp%2){
+                ans=(ans%mod*1ll*base%mod)%mod;
+            }
+            base=(base%mod*base%mod)%mod;
+            exp>>=1;
+        }
+        return ans;
+    }
     int numSubseq(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        long long int ans=0,mod=1e9+7,n=nums.size();
-        long long int dp[n];
-        dp[0]=1;
-        for(int i=1;i<n;i++){
-            dp[i]=(dp[i-1]%mod*2%mod)%mod;
-        }
-        int i=0,j=nums.size()-1;
-        while(i<nums.size() and j>0 and i<=j){
-            if(nums[i]+nums[j]>target){
-            j--;
-        }
-        else{
-            int l=j-i;
-            ans+=(dp[l])%mod;
-            i++;
+        long long int ans=0,mod=1e9+7,left=0,right=nums.size()-1;
+        while(left<=right){
+            if(nums[left]+nums[right]>target) right--;
+            else if(nums[left]+nums[right]<=target){
+                long long int dist=right-left;
+                long long int val=power(2,dist,mod);
+                ans=(ans%mod+val%mod)%mod;
+                left++;
             }
         }
-        return ans%mod;
+        return ans;
     }
 };
