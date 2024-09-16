62dbc66c05dc196cc080753835d3d7ec1bbb55f9
Parth Johri <76129377+ParthJohri@users.noreply.github.com>
2024-09-16 18:33:15 +0530

    Time: 6 ms (96.44%), Space: 17.8 MB (34.07%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    int convert(string& s){
-        int h=0,m=0;
-        string hh="",mm="";
-        bool flag=false;
-        for(auto i:s){
-            if(i==':'){
-                flag=true;
-                continue;
-            }
-            if(flag){
-                mm+=i;
-            }
-            else{
-                hh+=i;
-            }
-        }
-        h=stoi(hh);
-        m=stoi(mm);
-        return h*60+m;
-    }
     int findMinDifference(vector<string>& timePoints) {
-        vector<int> v;
+        vector<pair<int,int>> v;
         for(auto i:timePoints){
-            v.push_back(convert(i));
+            string hour = i.substr(0,2);
+            string minute = i.substr(3,2);
+            int h = stoi(hour), m = stoi(minute);
+            v.push_back({h,m});
         }
-        // Think of clockwise
-        sort(v.begin(),v.end());
-        v.push_back(v[0]+24*60);
-        int ans=INT_MAX;
-        for(int i=0;i<v.size()-1;i++){
-            int d=v[i+1]-v[i];
-            ans=min(ans,d);
-        }
-        return ans;
+            sort(v.begin(),v.end(),[&](pair<int,int>&a,pair<int,int>&b){
+                if(a.first==b.first) return a.second<b.second;
+                return  a.first<b.first;
+            });
+            int ans=INT_MAX;
+            v.push_back({v[0].first+24,v[0].second});
+            for(int i=0;i<v.size()-1;i++)
+            {
+                int hd=abs(v[i+1].first-v[i].first)*60;
+                int md=v[i+1].second-v[i].second;
+                ans=min(ans,hd+md);
+            }
+            return ans;
     }
 };
